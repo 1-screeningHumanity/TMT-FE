447565a0bc9b0cef27200016285f66b5598cbb15
jmlee119 <jmlee119@naver.com>
2024-05-21 10:49:03 +0900

    feat : 주식차트 거래량 추가

--- src/app/stock/page.tsx
+++ src/app/stock/page.tsx
@@ -1,6 +1,7 @@
 import Headers from '@/components/ui/Headers'
 import StockNamePrice from '@/components/pages/stock/StockNamePrice'
 import Charts from '@/components/pages/stock/Charts'
+import Charts2 from '@/components/pages/stock/Charts2'
 import OptionButtons from '@/components/pages/stock/OptionButtons'
 import CompanyInfo from '@/components/pages/stock/CompanyInfo'
 
@@ -10,6 +11,7 @@ export default function Page() {
       <Headers />
       <StockNamePrice />
       <Charts />
+      {/* <Charts2 /> */}
       <OptionButtons />
       <CompanyInfo />
     </main>
