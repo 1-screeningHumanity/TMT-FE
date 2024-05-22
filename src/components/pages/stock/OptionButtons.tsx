140d4a8889ac76d1db5e559ca0e60a14b089caff
jmlee119 <jmlee119@naver.com>
2024-05-22 16:17:25 +0900

    fix : 호가, 차트 -> 일,월

--- src/components/pages/stock/OptionButtons.tsx
+++ src/components/pages/stock/OptionButtons.tsx
@@ -1,23 +1,32 @@
 export default function OptionButtons() {
   return (
-    <div className="flex justify-between mt-5">
+    <div
+      className="flex justify-between items-center mt-5 w-full h-16 rounded-2xl"
+      style={{ backgroundColor: '#f2f2f2' }}
+    >
       <button
-        className="w-1/3 h-10 text-white mr-1 rounded-2xl"
+        className="w-1/4 h-10 text-white mr-1 rounded-2xl"
         style={{ backgroundColor: '#7D00D0' }}
       >
-        호가
+        일
       </button>
       <button
-        className="w-1/3 h-10 text-white mr-1 rounded-2xl"
+        className="w-1/4 h-10 text-white mr-1 rounded-2xl"
         style={{ backgroundColor: '#7D00D0' }}
       >
-        차트
+        월
       </button>
       <button
-        className="w-1/3 h-10 text-white rounded-2xl"
+        className="w-1/4 h-10 text-white rounded-2xl mr-1"
         style={{ backgroundColor: '#7D00D0' }}
       >
-        투자자
+        주
+      </button>
+      <button
+        className="w-1/4 h-10 text-white rounded-2xl"
+        style={{ backgroundColor: '#7D00D0' }}
+      >
+        년
       </button>
     </div>
   )
