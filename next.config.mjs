031add06ea44ab9b58e1498b70ce65dfe0c49005
sw-devr <sw4689@naver.com>
2024-06-02 03:02:23 +0900

    feat : 환경변수 설정

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ['img.icons8.com'],
   },
-}
+  env : {
+    API_BASE_URL : process.env.API_BASE_URL,
+    NEXTAUTH_SECRET : process.env.NEXTAUTH_SECRET,
+    KAKAO_CLIENT_ID : process.env.KAKAO_CLIENT_ID,
+    KAKAO_CLIENT_SECRET : process.env.KAKAO_CLIENT_SECRET,
+    NAVER_CLIENT_ID : process.env.NAVER_CLIENT_ID,
+    NAVER_CLIENT_SECRET : process.env.NAVER_CLIENT_SECRET,
+  }
+};
 
 export default nextConfig
