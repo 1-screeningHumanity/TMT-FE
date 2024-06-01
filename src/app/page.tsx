5c78b47c720d35abb944375da6d744c59d08c516
sw-devr <sw4689@naver.com>
2024-06-02 03:26:36 +0900

    feat : 로그아웃 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import 'regenerator-runtime/runtime'
 import SearchBar from '@/components/ui/SearchBar'
+import ButtonOfSignout from '@/components/ui/buttons/ButtonOfSignout'
 
 export default function Home() {
   return (
     <div>
       <SearchBar />
+      <h1 className='text-xl'>메인페이지입니다.</h1>
+      <ButtonOfSignout />
     </div>
   )
 }
