398d1581b34d1f59221ae887dcafec3f3cf3c506
sw-devr <sw4689@naver.com>
2024-06-04 11:48:53 +0900

    refactor : footer 코드 리팩토링

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { footer } from "@/lib/footer"
 
 export default function Footer() {
 
@@ -11,26 +12,12 @@ export default function Footer() {
 
   return (
   <div className="w-full fixed bottom-0 right-0 left-0 flex justify-around mb-2 mt-10">
-    <Link href="#" className="text-xs text-slate-400 flex items-center flex-col justify-center gap-2 w-16">
-      <Image width="30" height="30" src="https://img.icons8.com/material-outlined/24/sorting-answers.png" alt="sorting-answers" />
-      카테고리
+    {footer.map((data) => (
+    <Link href="#" className="text-xs text-slate-400 flex items-center flex-col justify-center gap-2 w-16" key={data.id}>
+      <Image width="30" height="30" src={data.src} alt={data.alt} />
+      {data.name}
     </Link>
-    <Link href="#" className="text-xs text-slate-400 flex items-center flex-col justify-center gap-2 w-16">
-      <Image width="30" height="30" src="https://img.icons8.com/ios/50/search--v1.png" alt="search--v1"/>
-      검색
-    </Link>
-    <Link href="#" className="text-xs text-slate-400 flex items-center flex-col justify-center gap-2 w-16">
-      <Image width="30" height="30" src="https://img.icons8.com/fluency-systems-regular/48/home--v1.png" alt="home--v1"/>
-      홈
-    </Link>
-    <Link href="#" className="text-xs text-slate-400 flex items-center flex-col justify-center gap-2 w-16">
-      <Image width="30" height="30" src="https://img.icons8.com/ios/50/bar-chart--v1.png" alt="bar-chart--v1"/>
-      순위
-    </Link>
-    <Link href="#" className="text-xs text-slate-400 flex items-center flex-col justify-center gap-2 w-16">
-      <Image width="30" height="30" src="https://img.icons8.com/parakeet-line/48/user.png" alt="user"/>
-      마이페이지
-    </Link>
+    ))}
   </div>
   )
 }
---
+++ src/lib/footer.ts
@@ -0,0 +1,33 @@
+
+export const footer = [
+  {
+    id : 1,
+    src : "https://img.icons8.com/material-outlined/24/sorting-answers.png",
+    alt : "sorting-answers",
+    name : "카테고리"
+  },
+  {
+    id : 2,
+    src : "https://img.icons8.com/ios/50/search--v1.png",
+    alt : "search--v1",
+    name : "검색"
+  },
+  {
+    id : 3,
+    src : "https://img.icons8.com/fluency-systems-regular/48/home--v1.png",
+    alt : "home--v1",
+    name : "홈"
+  },
+  {
+    id : 4,
+    src : "https://img.icons8.com/ios/50/bar-chart--v1.png",
+    alt : "bar-chart--v1",
+    name : "순위"
+  },
+  {
+    id : 5,
+    src : "https://img.icons8.com/parakeet-line/48/user.png",
+    alt : "user",
+    name : "마이페이지"
+  }
+]
