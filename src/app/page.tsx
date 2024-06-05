43772803ea5ccbecce54257232845a53b5176257
sw-devr <sw4689@naver.com>
2024-06-05 11:53:28 +0900

    feat : 메인페이지 로그인 버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
 import 'regenerator-runtime/runtime'
 import SearchBar from '@/components/ui/SearchBar'
 import ButtonOfSignout from '@/components/ui/buttons/ButtonOfSignout'
+import { getSession } from 'next-auth/react'
+import ButtonToSignin from '@/components/ui/buttons/ButtonToSignin';
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getSession();
+  console.log("session :", session);
+
   return (
     <div>
       <SearchBar />
       <h1 className='text-xl'>메인페이지입니다.</h1>
-      <ButtonOfSignout />
+      { session?.user == null ?
+        <ButtonToSignin />  :
+        <ButtonOfSignout /> 
+      }
+      <div className='text-3xl'>
+        {session?.user?.data?.name || "로그인이 필요합니다."}
+      </div>
     </div>
   )
 }
