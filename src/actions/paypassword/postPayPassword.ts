e7fb5be445904c5d3af7dcb12581a02ea4c6a4a6
sw-devr <sw4689@naver.com>
2024-06-05 11:47:25 +0900

    fix : api fetch url 오류 해결

--- src/actions/paypassword/postPayPassword.ts
+++ src/actions/paypassword/postPayPassword.ts
@@ -3,7 +3,7 @@
 export default async function postPayPassword(nickName : string | null, password : string | undefined) {
 
   try {
-    const res = await fetch(`process.env.API_BASE_URL/member/pay-password`, {
+    const res = await fetch(`${process.env.API_BASE_URL}/member/pay-password`, {
       cache: "no-store",
       method: "POST",
       headers: {
--- src/actions/signup/postSignup.ts
+++ src/actions/signup/postSignup.ts
@@ -3,7 +3,7 @@
 export async function postSignup(name : string | undefined, phoneNumber : string | undefined, password : string | undefined, nickName : string | undefined) {
 
   try {
-    const res = await fetch(`process.env.API_BASE_URL/member/signup`, {
+    const res = await fetch(`${process.env.API_BASE_URL}/member/signup`, {
       cache: "no-store",
       method: "POST",
       headers: {
