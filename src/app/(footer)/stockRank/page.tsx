f2e5870dad1d2ce97557947f789580691e9399a8
jmlee119 <jmlee119@naver.com>
2024-06-05 17:19:57 +0900

    feat : 급등주 조회

---
+++ src/app/(footer)/stockRank/page.tsx
@@ -0,0 +1,25 @@
+import { sortAPI } from '@/actions/stock/sort'
+import { dateFormmating } from '@/utils/time'
+import { StockSortType } from '@/types/StcokSortType'
+import formatNumberWithCommas from '@/utils/formatNumberWithCommas'
+import SortButton from '@/components/pages/stock/SortButton'
+import SortRank from '@/components/pages/stock/SortRank'
+// import { searchParams } from 'next/navigation'
+
+export default async function Page(props: any) {
+  let option = props?.searchParams.sort
+  if (option == undefined) {
+    option = 'soaring-stocks'
+  }
+  const sortResult = await sortAPI(option)
+
+  let date = dateFormmating(sortResult[0]?.dateTime)
+
+  return (
+    <main>
+      <SortButton />
+      <span className="ml-2 text-center">{date} 기준입니다.</span>
+      <SortRank sortResult={sortResult} />
+    </main>
+  )
+}
