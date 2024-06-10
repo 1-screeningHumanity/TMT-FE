ce7ba198ac1f430550b35e29009b11c1c98df653
jmlee119 <jmlee119@naver.com>
2024-06-10 17:55:41 +0900

    fix : 숫자 포맷팅

--- src/utils/formatNumberWithCommas.ts
+++ src/utils/formatNumberWithCommas.ts
@@ -1,6 +1,9 @@
-export default function formatNumberWithCommas(number: number | undefined) {
+export default function formatNumberWithCommas(
+  number: number | string | null | undefined,
+) {
   if (!number) {
     return 0
   }
+  number = Number(number)
   return number.toLocaleString()
 }
