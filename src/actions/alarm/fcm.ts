7327373311bf30fa5bd61130637e8a79ae6afa76
jmlee119 <jmlee119@naver.com>
2024-06-12 12:20:26 +0900

    feat : fcm 발급 api

---
+++ src/actions/alarm/fcm.ts
@@ -0,0 +1,26 @@
+import { DeleteAPI, GetAPI, PostAPI } from '../FetchAPI'
+import { getAccessToken } from '../tokens'
+
+const token = getAccessToken()
+async function fcmSendAPI(fcmToken: string) {
+  const res = await PostAPI(
+    '/notification/fcm-token',
+    { fcmToken },
+    await token,
+  )
+  return res
+}
+async function alarmListAPI() {
+  const res = await GetAPI('/notification', undefined, await token)
+  return res
+}
+async function alarmReadAPI(alarmId: string) {
+  const res = await PostAPI('/notification', { alarmId }, await token)
+  return res
+}
+async function alarmDeleteAPI(alarmId: string[]) {
+  const res = await DeleteAPI('/notification', { alarmId }, await token)
+  return res
+}
+
+export { fcmSendAPI, alarmListAPI, alarmReadAPI, alarmDeleteAPI }
