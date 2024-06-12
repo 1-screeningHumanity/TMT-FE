095af9446c383ffa3fa416558e33fa2b9712aa52
sw-devr <sw4689@naver.com>
2024-06-12 22:34:45 +0900

    feat : 결제비밀번호 페이지 버튼 경로 설정

--- src/components/pages/password/PayPasswordChange.tsx
+++ src/components/pages/password/PayPasswordChange.tsx
@@ -20,12 +20,12 @@ export default function PayPasswordChange ({setPassword} : {setPassword : Dispat
   return(
     <>
       <div className="flex mx-10 justify-between my-10 items-center">
-        <div className="rounded-full bg-[#f6f7f9] flex justify-center items-center w-4 h-4" >
-          <Image width="20" height="20" src="https://img.icons8.com/ios-glyphs/20/back.png" alt="back"/>
+        <div className="rounded-full bg-[#f6f7f9] flex justify-center items-center w-4 h-4" onClick={()=> history.back()}>
+          <Image width="20" height="20" src="https://img.icons8.com/ios-glyphs/20/back.png" alt="back" />
         </div>
         <h1 className="text-lg text-[#7d00d0] font-extrabold">결제비밀번호 변경</h1>
-        <div className="rounded-full bg-[#f6f7f9] flex justify-center items-center w-5 h-5">
-            <Image width="20" height="20" src="https://img.icons8.com/ios/20/000000/multiply.png" alt="cancel"/>
+        <div className="rounded-full bg-[#f6f7f9] flex justify-center items-center w-5 h-5" onClick={()=> location.href="/mypage"}>
+            <Image width="20" height="20" src="https://img.icons8.com/ios/20/000000/multiply.png" alt="cancel" />
         </div>
       </div>
       <div className="text-center mt-24 mb-20">
