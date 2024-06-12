63dd3ca39fa21eb1ced432bb4beedd9983f7af0d
sw-devr <sw4689@naver.com>
2024-06-12 23:14:22 +0900

    feat : 구독자 목록 페이지 구현

---
+++ src/app/(footer)/subscribe/follower/page.tsx
@@ -0,0 +1,21 @@
+import Headers from "@/components/ui/Headers";
+import SubscribeBox from "@/components/ui/SubscribeBox";
+
+export default function Follower(){
+  return (
+    <section>
+      <Headers title="구독자 목록"/>
+      <div className="text-center w-40 h-10 leading-10 mx-auto mt-4 text-white bg-[#7d00d0] font-[Pretendard-Regular]">나를 구독한 사람 목록</div>
+      <SubscribeBox />
+      <SubscribeBox />
+      <SubscribeBox />
+      <SubscribeBox />
+      <SubscribeBox />
+      <SubscribeBox />
+      <SubscribeBox />
+      <SubscribeBox />
+      <SubscribeBox />
+      <div className="mb-40"></div>
+    </section>
+  )
+}
