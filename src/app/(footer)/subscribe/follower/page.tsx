57a977f3d5f8a95fbfce1620f7fce4c5e452e8d2
sw-devr <sw4689@naver.com>
2024-06-13 11:33:31 +0900

    fix : 구독자 페이지 서브타이틀 분리

--- src/app/(footer)/subscribe/follower/page.tsx
+++ src/app/(footer)/subscribe/follower/page.tsx
@@ -1,11 +1,12 @@
 import Headers from "@/components/ui/Headers";
+import SubTitleOfSubScribe from "@/components/ui/SubTitleOfSubScribe";
 import SubscribeBox from "@/components/ui/SubscribeBox";
 
 export default function Follower(){
   return (
     <section>
       <Headers title="구독자 목록"/>
-      <div className="text-center w-40 h-10 leading-10 mx-auto mt-4 text-white bg-[#7d00d0] font-[Pretendard-Regular]">나를 구독한 사람 목록</div>
+      <SubTitleOfSubScribe title="나를 구독한 사람 목록"/>
       <SubscribeBox />
       <SubscribeBox />
       <SubscribeBox />
