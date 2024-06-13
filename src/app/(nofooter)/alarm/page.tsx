5507f9b913ef02522919b656ad78d0fc7a45c69a
jmlee119 <jmlee119@naver.com>
2024-06-13 12:21:23 +0900

    feat : 알림 프롭스 전달

--- src/app/(nofooter)/alarm/page.tsx
+++ src/app/(nofooter)/alarm/page.tsx
@@ -1,18 +1,21 @@
 import Headers from '@/components/ui/Headers'
 import IsReadCheck from '@/components/pages/alarm/IsReadCheck'
-import { alarmListAPI } from '@/actions/alarm/fcm'
+import { alarmListAPI } from '../../../actions/alarm/fcm'
 import AlarmList from '@/components/pages/alarm/AlarmList'
+import { AlarmListData } from '@/lib/alarm/AlarmList'
 
-export default async function Alarm() {
+export default async function Alarm(props: any) {
+  console.log('props in alarm.tsx :', props)
   const alarmList = await alarmListAPI()
-  console.log(alarmList)
+  // const alarmList = await AlarmListData
+  console.log('alarmList', alarmList)
 
   return (
     <>
       <Headers title="알림센터" />
       <hr />
       <IsReadCheck />
-      <AlarmList />
+      <AlarmList alarmList={alarmList.data} />
     </>
   )
 }
