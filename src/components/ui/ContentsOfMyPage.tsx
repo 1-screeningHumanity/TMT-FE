5af4b205fb3d251e1e022228b6874df3be7ed29e
sw-devr <sw4689@naver.com>
2024-06-13 10:02:28 +0900

    fix : ContentsOfMypage 수정

--- src/components/ui/ContentsOfMyPage.tsx
+++ src/components/ui/ContentsOfMyPage.tsx
@@ -1,18 +1,26 @@
-'use client';
+import Link from "next/link";
 
 export default function ContentsOfMyPage(){
+
+  const contents = [
+    {id : 1, name : "비밀번호 변경", path : "/change/password"},
+    {id : 2, name : "결제 비밀번호 변경", path : "/change/paypassword"},
+    {id : 3, name : "닉네임 변경", path : "/change/nickname"},
+    {id : 4, name : "결제하기", path : "/payments"},
+    {id : 5, name : "금액(원)충전하기", path : "/charge"},
+    {id : 6, name : "매수/매도 내역 조회", path : "/trade/tradelists"},
+    {id : 7, name : "예약 매수/매도 내역 조회", path : "/trade/reservation/tradelists"},
+    {id : 8, name : "구독자 조회", path : "/subscribe/following"},
+    {id : 9, name : "나를 구독한 사람 조회", path : "/subscribe/follower"},
+    {id : 10, name : "즐겨찾기 목록 조회", path : "/bookmark"},
+  ]
+
+
   return (
     <div className="w-80 mx-auto mt-40">
-      <button className="border-[1px] w-80 h-10" onClick={()=> location.href="/change/password"}>비밀번호 변경</button>
-      <button className="border-[1px] w-80 h-10" onClick={()=> location.href="/change/paypassword"}>결제 비밀번호 변경</button>
-      <button className="border-[1px] w-80 h-10" onClick={()=> location.href="/change/nickname"}>닉네임 변경</button>
-      <button className="border-[1px] w-80 h-10" onClick={()=> location.href="/payments"}>결제하기</button>
-      <button className="border-[1px] w-80 h-10" onClick={()=> location.href="/charge"}>금액(원)충전하기</button>
-      <button className="border-[1px] w-80 h-10" onClick={()=> location.href="/trade/tradelists"}>매수/매도 내역 조회</button>
-      <button className="border-[1px] w-80 h-10" onClick={()=> location.href="/trade/reservation/tradelists"}>예약 매수/매도 내역 조회</button>
-      <button className="border-[1px] w-80 h-10" onClick={()=> location.href="/subscribe/following"}>구독자 조회</button>
-      <button className="border-[1px] w-80 h-10" onClick={()=> location.href="/subscribe/follower"}>나를 구독한 사람 조회</button>
-      <button className="border-[1px] w-80 h-10" onClick={()=> location.href="/bookmark"}>즐겨찾기 목록 조회</button>
+      {contents.map((content) => (
+        <Link key={content.id} href={content.path} className="border-[1px] w-80 h-12 block text-center leading-10">{content.name}</Link>
+      ))}
     </div>
   )
 }
