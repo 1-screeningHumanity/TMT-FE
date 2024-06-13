90c8077bb4162e2aaec3a6a06e1993088ea33ee6
sw-devr <sw4689@naver.com>
2024-06-13 10:06:16 +0900

    fix : formatNumberWithCommas util 사용

--- src/components/ui/TitleHOne.tsx
+++ src/components/ui/TitleHOne.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import formatNumberWithCommas from '@/utils/formatNumberWithCommas'
 
 function TitleHOne({title, cash}:{title:string, cash:number}) {
   return (
     <h1 className="text-center mt-14 text-lg font-bold">{
-      title === "교환하기" ? `현재 보유 원 : ${cash.toLocaleString()} 원` : `총 결제금액 : ${cash.toLocaleString()} 캐시(￦)`
+      title === "교환하기" ? `현재 보유 원 : ${formatNumberWithCommas(cash)} 원` : `총 결제금액 : ${formatNumberWithCommas(cash)} 캐시(￦)`
     }</h1>
   )
 }
