e92993297643d4576254ee113f36a2ad1d78ba9b
sw-devr <sw4689@naver.com>
2024-06-14 17:01:26 +0900

    fix : 결제페이지 card 코드 수정

--- src/components/pages/payment/CashCard.tsx
+++ src/components/pages/payment/CashCard.tsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import Card from './Card'
+'use client'
 
-const dummy = [
-  {id:1, cash:"1,000"},
-  {id:2, cash:"5,000"},
-  {id:3, cash:"10,000"},
-  {id:4, cash:"50,000"},
-]
+import React, { useState } from 'react'
+import Card from './Card'
+import { useRouter } from 'next/navigation'
+import { CashCardData } from '@/lib/payments/CashCardData'
 
 function CashCard() {
+
+  const [cash, setCash] = useState<number>(0)
+
+  function getCash(amount : number){
+    setCash(amount)
+    console.log(amount)
+  }
+
+
   return (
     <div className="grid grid-cols-2 mt-20 gap-4 w-5/6 mx-auto">
-       {
-          dummy.map((item) => (
-            <Card key={item.id} cash={item.cash}/>
-          ))
-       } 
-      </div>
+      {CashCardData.map((item) => (
+        <Card key={item.id} cash={item.cash} onClick={()=> getCash(item.cash)}/>
+      ))}
+    </div>
   )
 }
 
