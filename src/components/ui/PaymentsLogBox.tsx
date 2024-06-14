0eed8dd58deca810b73e409aa774da67a7c2b6bd
sw-devr <sw4689@naver.com>
2024-06-14 17:02:08 +0900

    feat : 충전내역 조회 페이지 구현 2

---
+++ src/components/ui/PaymentsLogBox.tsx
@@ -0,0 +1,25 @@
+import formatNumberWithCommas from "@/utils/formatNumberWithCommas";
+import { parseDateTime } from "@/utils/parseDate";
+
+export default function PaymentsLogBox({createdAt, payName, totalAmount}: PaymentsLogBoxProps){
+
+  const parsed = parseDateTime(createdAt);
+  
+  return (
+    <div className="w-full border-y-[0.5px] mx-auto h-24 rounded-md flex items-center tracking-tight">
+      <div>
+        <span className="font-bold mx-5 text-lg font-[Pretendard-Regular]">{parsed.datePart}</span>
+      </div>
+      <div className="w-3/5 ">
+        <p className="font-bold">캐시 충전</p>
+        <div>
+          <span className="text-xs text-[#775da6]">{parsed.timePart} |</span> 
+          <span className="text-xs text-[#775da6]">{ `${payName}` == `KakaoPay` ? " 카카오 페이" : " 카드 결제"}</span>
+        </div>
+      </div>
+      <div className="mx-7 text-[#7d00d0] font-bold">
+        ￦{formatNumberWithCommas(totalAmount)}
+      </div>
+    </div>
+  )
+}
---
+++ src/lib/payments/PaymentsLogLists.ts
@@ -0,0 +1,38 @@
+export const PaymentsLogLists = [
+  {"createdAt" : "2024-12-13T22:01:45.404337",
+    "payName" : "Kakaopay",
+    "totalAmount" : 10000
+  },
+  {"createdAt" : "2024-11-12T12:45:45.404337",
+    "payName" : "Kakaopay",
+    "totalAmount" : 100000
+  },
+  {"createdAt" : "2024-10-11T10:05:45.404337",
+    "payName" : "Kakaopay",
+    "totalAmount" : 50000
+  },
+  {"createdAt" : "2024-09-21T08:05:45.404337",
+    "payName" : "카드 결제",
+    "totalAmount" : 50000
+  },
+  {"createdAt" : "2024-08-08T10:05:45.404337",
+    "payName" : "Kakaopay",
+    "totalAmount" : 500000
+  },
+  {"createdAt" : "2024-07-01T10:05:45.404337",
+    "payName" : "Kakaopay",
+    "totalAmount" : 1000000
+  },
+  {"createdAt" : "2024-06-21T10:05:45.404337",
+    "payName" : "Kakaopay",
+    "totalAmount" : 50000
+  },
+  {"createdAt" : "2024-06-21T10:05:45.404337",
+    "payName" : "Kakaopay",
+    "totalAmount" : 50000
+  },
+  {"createdAt" : "2024-06-21T10:05:45.404337",
+    "payName" : "Kakaopay",
+    "totalAmount" : 50000
+  },
+]
