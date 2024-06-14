2a51735c6aa1640cc52a69961682bfe9062a4f45
sw-devr <sw4689@naver.com>
2024-06-14 16:41:50 +0900

    fix : location.href -> useRouter로 변경

--- src/app/(nofooter)/change/paypassword/page.tsx
+++ src/app/(nofooter)/change/paypassword/page.tsx
@@ -6,12 +6,15 @@ import PayPasswordChange from "@/components/pages/password/PayPasswordChange";
 import patchPaypasswordChange from "@/actions/change/patchPaypasswordChange";
 import { Session } from "next-auth";
 import { getSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function paypassword (){
   const [password, setPassword] = useState<string>("");
   const [checkPassword, setCheckPassword] = useState<string>("");
   const [step, setStep] = useState<boolean>(true);
   const [session, setSession] = useState<Session | null>(null);
+
+  const router = useRouter();
 
   function handleStep(){
     if(password.length === 4){
@@ -24,10 +27,9 @@ export default function paypassword (){
   async function handleSetPayPassword(){
     if(password === checkPassword && password.length === 4 && checkPassword.length === 4){
       const response = await patchPaypasswordChange(checkPassword)
-      console.log("response:", response);
       if(response.code == 200){
         alert("비밀번호가 변경되었습니다.");
-        location.href="/change/paypassword/complete";
+        router.push("/change/paypassword/complete");
       }
     }else if (checkPassword.length !== 4){
       alert("비밀번호를 4자리 입력해주세요.");
@@ -45,7 +47,7 @@ export default function paypassword (){
           <input type="button" className="rounded-lg w-80 h-14 font-bold text-center mx-2 text-white bg-[#7d00d0]" value={"다음으로"} onClick={() => handleStep()}/>
         </div>) :
         (<div className="w-fit mx-auto absolute bottom-6 left-0 right-0">
-          <input type="button" className="rounded-lg w-40 h-14 font-bold text-center mx-2 text-white bg-black" value={"이전으로"} onClick={() => history.go(-1)}/>
+          <input type="button" className="rounded-lg w-40 h-14 font-bold text-center mx-2 text-white bg-black" value={"이전으로"} onClick={() => router.back()}/>
           <input type="button" className="rounded-lg w-40 h-14 font-bold text-center mx-2 text-white bg-[#7d00d0]" value={"완료"} onClick={() => {handleSetPayPassword() }}/>
         </div>)
       }
