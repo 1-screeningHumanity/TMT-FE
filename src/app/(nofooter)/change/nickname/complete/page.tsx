12a0adc4cca56765aff042f38ecff51516a4c348
sw-devr <sw4689@naver.com>
2024-06-14 16:45:17 +0900

    fix : 완료 페이지 버튼 수정

--- src/app/(nofooter)/change/nickname/complete/page.tsx
+++ src/app/(nofooter)/change/nickname/complete/page.tsx
@@ -1,18 +1,18 @@
 'use client'
 
+import ButtonOfSignout from "@/components/ui/buttons/ButtonOfSignout";
+import ButtonToMyPage from "@/components/ui/buttons/ButtonToMyPage";
 import Image from "next/image";
 
 export default function complete (){
   return(
     <section>
       <Image src={"/assets/images/logo3.svg"} alt="logo3" width={300} height={200} className="mx-auto pr-20 mt-60 mb-6"/>
-      <div className="text-lg font-bold w-80 mx-auto">
+      <div className="text-xl font-bold w-80 mx-auto">
         닉네임 변경완료 <br/>
         <span className="text-xs text-[#3a3f52] w-80 mx-auto">닉네임이 변경되었습니다</span>
       </div>
-      <button className="bg-[#7d00d0] text-white text-md font-semibold rounded-lg w-80 h-10 mx-auto block my-20 font-[Pretendard-Regular]" onClick={()=> location.href="/"} >
-        메인페이지로
-      </button>
+      <ButtonToMyPage />
     </section>
   )
 }
--- src/app/(nofooter)/change/paypassword/complete/page.tsx
+++ src/app/(nofooter)/change/paypassword/complete/page.tsx
@@ -1,8 +1,11 @@
 'use client'
 
+import ButtonToMyPage from "@/components/ui/buttons/ButtonToMyPage";
 import Image from "next/image";
 
 export default function complete (){
+
+
   return(
     <section>
       <Image src={"/assets/images/logo3.svg"} alt="logo3" width={300} height={200} className="mx-auto pr-20 mt-60 mb-6"/>
@@ -10,9 +13,7 @@ export default function complete (){
         결제비밀번호 변경완료 <br/>
         <span className="text-xs text-[#3a3f52] w-80 mx-auto">결제비밀번호가 변경되었습니다</span>
       </div>
-      <button className="bg-[#7d00d0] text-white text-md font-semibold rounded-lg w-80 h-10 mx-auto block my-20 font-[Pretendard-Regular]" onClick={()=> location.href="/"} >
-        메인페이지로
-      </button>
+      <ButtonToMyPage />
     </section>
   )
 }
