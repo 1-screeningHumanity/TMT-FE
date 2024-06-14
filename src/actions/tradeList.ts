b656252c25b53505915dd3c6e03e507bde7e67b0
sw-devr <sw4689@naver.com>
2024-06-14 16:35:55 +0900

    feat : 매수/매도, 예약 매수/매도 api 추가

---
+++ src/actions/tradeList.ts
@@ -0,0 +1,49 @@
+"use server"
+
+import { getAccessToken } from "./tokens";
+
+export async function getTradeList() {
+
+  try {
+    const TOKEN = await getAccessToken();
+    const res = await fetch(`${process.env.API_BASE_URL}/trade/trade-lists`, {
+      cache: "no-store",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization" : TOKEN
+      },
+      },
+    )
+    if (res.ok) {
+      const data = await res.json()
+      console.log(data);
+      return data
+    }
+  } catch (error) {
+    return
+  }
+}
+
+export async function getReservationTradeList() {
+
+  try {
+    const TOKEN = await getAccessToken();
+    const res = await fetch(`${process.env.API_BASE_URL}/trade/reservation/trade-lists`, {
+      cache: "no-store",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization" : TOKEN
+      },
+      },
+    )
+    if (res.ok) {
+      const data = await res.json()
+      console.log(data);
+      return data
+    }
+  } catch (error) {
+    return
+  }
+}
