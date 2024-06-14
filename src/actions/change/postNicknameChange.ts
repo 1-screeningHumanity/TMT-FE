1dc67488650688993e899badd685b7faf2305b53
sw-devr <sw4689@naver.com>
2024-06-14 16:36:44 +0900

    fix : 닉네임 변경 api 수정

--- src/actions/change/postNicknameChange.ts
+++ src/actions/change/postNicknameChange.ts
@@ -6,12 +6,13 @@ import { getAccessToken } from "../tokens";
 export default async function postNicknameChange(nickname : string | null) {
 
   try {
-    const res = await fetch(`${process.env.API_BASE_URL}/member/pay-password`, {
+    const TOKEN = await getAccessToken();
+    const res = await fetch(`${process.env.API_BASE_URL}/member/nickname`, {
       cache: "no-store",
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization" : await getAccessToken()
+        "Authorization" : TOKEN
       },
       body : JSON.stringify({
         nickname : nickname,
