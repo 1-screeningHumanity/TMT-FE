aa3f6d9e58dd202b0fb98c1eaaca20fe5cda31be
sw-devr <sw4689@naver.com>
2024-06-14 16:55:32 +0900

    feat : 로그아웃 버튼 api호출 추가

--- src/components/ui/buttons/ButtonOfSignout.tsx
+++ src/components/ui/buttons/ButtonOfSignout.tsx
@@ -1,8 +1,14 @@
 'use client'
 
+import { postSignOut } from "@/actions/member";
 import { signOut } from "next-auth/react";
 
 export default function ButtonOfSignout(){
 
-  return <button className="bg-[#7d00d0] text-white text-sm font-semibold rounded-lg w-80 h-10 mx-auto block my-6" onClick={() => signOut()}>로그아웃</button>
+  const handleSignOut = async () => {
+    const res = await postSignOut();
+    await signOut();
+  }
+
+  return <button className="bg-[#7d00d0] text-white text-sm font-semibold rounded-lg w-80 h-10 mx-auto block my-6" onClick={() => handleSignOut()}>로그아웃</button>
 }
