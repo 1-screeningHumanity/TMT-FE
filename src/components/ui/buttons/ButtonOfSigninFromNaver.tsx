1245e862c3a7cb6b48a9a3331bcfa3449c274da1
sw-devr <sw4689@naver.com>
2024-06-17 22:24:25 +0900

    fix : 로그인 페이지 수정

--- src/components/ui/buttons/ButtonOfSigninFromNaver.tsx
+++ src/components/ui/buttons/ButtonOfSigninFromNaver.tsx
@@ -4,5 +4,5 @@ import { signIn } from "next-auth/react"
 
 export default function ButtonOfSigninFromNaver(){
 
-  return <button className="w-28 rounded-md h-10 bg-green-500 flex justify-center items-center text-white font-bold text-xl" onClick={() => {signIn("naver", { redirect: true, callbackUrl: "/" })}}>N</button>
+  return <button className="w-[48%] rounded-md h-10 bg-green-500 flex justify-center items-center text-white font-bold text-xl" onClick={() => {signIn("naver", { redirect: true, callbackUrl: "/" })}}>N</button>
 }
