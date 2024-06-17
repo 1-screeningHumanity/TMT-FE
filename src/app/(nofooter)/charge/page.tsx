2250649afd79127d2086492ae6a051c731ff1861
sw-devr <sw4689@naver.com>
2024-06-17 22:28:27 +0900

    feat : 충전하기 기능 구현

--- src/app/(nofooter)/charge/page.tsx
+++ src/app/(nofooter)/charge/page.tsx
@@ -1,46 +1,40 @@
-'use client'
-
+import { cashInfoAPI, wonInfoAPI } from "@/actions/wallet";
 import CashCard from "@/components/pages/payment/CashCard";
+import WonCard from "@/components/pages/payment/WonCard";
 import Headers from "@/components/ui/Headers";
 import ButtonOfCharge from "@/components/ui/buttons/ButtonOfCharge";
+import formatNumberWithCommas from "@/utils/formatNumberWithCommas";
 import Image from "next/image";
-import { useState } from "react";
+import Link from "next/link";
 
-export default function charge(){
+export default async function charge({searchParams} : {searchParams: {[key: string]: string}}){
 
-  const [totalCash, setTotalCash] = useState<number>(0); 
-  const [totalAmount, setTotalAmount] = useState<number>(0);
+  const price = searchParams.price
 
-  const handleTotalAmount = (e : React.ChangeEvent<HTMLInputElement>) => {
-    setTotalAmount(Number(e.target.value));
-  };
-
+  const resWon = await wonInfoAPI();
+  const wonInfo = resWon?.data.won;
+  const resCash = await cashInfoAPI();
+  const cashInfo = resCash?.data.cash;
 
   return (
     <section>
       <Headers title="교환하기"/>
+      <div className="w-60 mx-auto text-center">
+        <h3 className="mt-10 mb-8 text-sm flex items-end">보유 캐시 : <span className="text-2xl text-[#7d00d0]">{formatNumberWithCommas(cashInfo)} 캐시</span></h3>
+      </div>
+      <WonCard />
 
-      
-      <h1 className="text-center mt-8 text-lg font-bold">현재 보유 원 : {"0"} 원</h1>
-      <CashCard />
-      {/* <div className="grid grid-cols-2 mt-20 gap-4 w-5/6 mx-auto">
-        <div className="border-[1px] w-40 h-20 flex items-center justify-center flex-col gap-2 hover:bg-slate-200" onClick={()=>{setTotalAmount(100000); setTotalCash(1000);}}>100,000 원<span className="text-xs text-slate-400">1,000캐시</span></div>
-        <div className="border-[1px] w-40 h-20 flex items-center justify-center flex-col gap-2 hover:bg-slate-200" onClick={()=>{setTotalAmount(500000); setTotalCash(5000);}}>500,000 원<span className="text-xs text-slate-400">5,000캐시</span></div>
-        <div className="border-[1px] w-40 h-20 flex items-center justify-center flex-col gap-2 hover:bg-slate-200" onClick={()=>{setTotalAmount(1000000); setTotalCash(10000);}}>1,000,000 원<span className="text-xs text-slate-400">10,000캐시</span></div>
-        <div className="border-[1px] w-40 h-20 flex items-center justify-center flex-col gap-2 hover:bg-slate-200" onClick={()=>{setTotalAmount(5000000); setTotalCash(50000);}}>5,000,000 원<span className="text-xs text-slate-400">50,000캐시</span></div>
-      </div> */}
 
-      <h1 className="mt-14 text-center text-xl font-bold mb-10">차감될 캐시 : {totalCash.toLocaleString()} 캐시</h1>
-      <h1 className="mt-10 text-center text-xl font-bold mb-36">교환 금액 : {totalAmount.toLocaleString()} 원</h1>
-      <div className="relative" onClick={() => location.href="/payments"}>
+      <h3 className="mt-16 mb-14 text-sm flex items-end justify-center h-10 mx-auto">충전 후 금액 : <span className="text-2xl text-[#7d00d0]">{formatNumberWithCommas(wonInfo + (Number(price) * 100))} 원</span></h3>
+      <Link href={"/payments"}>
         <div className="absolute right-6 flex flex-col items-center">
           <div className="w-14 h-14 rounded-full bg-yellow-300 flex items-center justify-center ">
             <Image src="/assets/images/nextPageIcon.svg" alt="nextPage" width={30} height={30}/>
           </div>
-          <p className="text-xs mt-2 text-slate-400 animate-bounce">충전하러가기</p> 
+          <p className="text-xs mt-2 text-slate-400 animate-bounce">충전하기</p> 
         </div>
-      </div>
-      <ButtonOfCharge />
+      </Link>
+      <ButtonOfCharge price={price} cash={cashInfo}/>
     </section>
   )
 }
