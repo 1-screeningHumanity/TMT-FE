a7718b4f36e4c8608e165d1e0d612fe0f8b9b159
jmlee119 <jmlee119@naver.com>
2024-06-18 09:43:26 +0900

    feat : kospi, kosdaq api 추가

--- src/actions/stock/sort.ts
+++
@@ -1,7 +0,0 @@
-import { GetAPI } from '../fetchAPI'
-
-async function sortAPI(sortOption: string) {
-  const res = await GetAPI(`/stockitem/mainpage/${sortOption}`)
-  return res.data
-}
-export { sortAPI }
