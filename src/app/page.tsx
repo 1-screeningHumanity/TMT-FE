42f51ac5a18a31974bb17274fa1989dbac89f2ec
jmlee119 <jmlee119@naver.com>
2024-06-18 12:00:54 +0900

    feat : 메인 화면 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,22 @@ import SearchBarUI from '@/components/ui/SearchBarUI'
 import Headers from '@/components/ui/Headers'
 import Footer from '@/components/ui/Footer'
 import Kospi from '@/components/pages/mainpages/Kospi'
-import { KosdaqAPI, KospiAPI } from '@/actions/stock/mainpage'
+import { KosdaqAPI, KospiAPI, sortAPI } from '@/actions/stock/mainpage'
 import { kospiType } from '@/types/Mainpage'
 import Kosdaq from '@/components/pages/mainpages/Kosdaq'
 import { dateFormmating } from '@/utils/time'
+import Soaring from '@/components/pages/mainpages/Soaring'
+import { StockSortType } from '@/types/StcokSortType'
+import Link from 'next/link'
 
 export default async function Home() {
   const kospiData: kospiType = await KospiAPI()
   const kosdaqData: kospiType = await KosdaqAPI()
+  const soaringData = await sortAPI('soaring-stocks')
+  const plummetingData = await sortAPI('plummeting-stocks')
+  const top3soaring: StockSortType[] = soaringData.slice(0, 3)
+  const top3plummeting: StockSortType[] = plummetingData.slice(0, 3)
+
   const date = dateFormmating(kospiData.dateTime)
 
   return (
@@ -23,7 +31,17 @@ export default async function Home() {
         <Kospi data={kospiData} />
         <Kosdaq data={kosdaqData} />
       </div>
-
+      <span className="font-bold text-xl m-3">오늘의 급등주 Top3</span>
+      <Link
+        href={{ pathname: '/stockRank', query: { sort: 'soaring-stocks' } }}
+      >
+        <Soaring data={top3soaring} color={'red'} />
+      </Link>
+      <Link
+        href={{ pathname: '/stockRank', query: { sort: 'plummeting-stocks' } }}
+      >
+        <Soaring data={top3plummeting} color={'blue'} />
+      </Link>
       <Footer />
     </div>
   )
---
+++ src/components/pages/mainpages/Soaring.tsx
@@ -0,0 +1,39 @@
+import { StockSortType } from '@/types/StcokSortType'
+import formatNumberWithCommas from '@/utils/formatNumberWithCommas'
+
+export default function Soaring({
+  data,
+  color,
+}: {
+  data: StockSortType[]
+  color: string
+}) {
+  const SoaringData = data
+  return (
+    <section
+      className="m-3 bg-red-100 rounded-xl"
+      style={{
+        background:
+          color === 'red' ? ' rgba(255, 0, 0, 0.1)' : ' rgba(0, 0, 255, 0.1)',
+      }}
+    >
+      <div className="w-full h-12 flex justify-center items-center text-center border-b-2 border-white font-semibold">
+        <span className="w-1/12"></span>
+        <span className="w-5/12">종목명</span>
+        <span className="w-3/12">현재가</span>
+        <span className="w-3/12">등락률</span>
+      </div>
+      {SoaringData.map((soar) => (
+        <ul
+          className="w-full h-auto min-h-8 flex justify-center items-center text-center"
+          key={soar.id}
+        >
+          <li className="w-1/12">{soar.data_rank}</li>
+          <li className="w-5/12 break-words">{soar.hts_kor_isnm}</li>
+          <li className="w-3/12">{formatNumberWithCommas(soar.stck_prpr)}</li>
+          <li className="w-3/12">{soar.prdy_ctrt}</li>
+        </ul>
+      ))}
+    </section>
+  )
+}
