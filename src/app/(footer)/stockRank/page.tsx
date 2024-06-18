df29af9199aaf46368dbe80481cb432559891907
jmlee119 <jmlee119@naver.com>
2024-06-18 09:37:54 +0900

    feat : 주식 순위 서버사이드

--- src/app/(footer)/stockRank/page.tsx
+++ src/app/(footer)/stockRank/page.tsx
@@ -7,18 +7,21 @@ import SortRank from '@/components/pages/stock/SortRank'
 import Headers from '@/components/ui/Headers'
 // import { searchParams } from 'next/navigation'
 
-export default async function Page(props: any) {
-  let option = props?.searchParams.sort
-  if (option == undefined) {
-    option = 'soaring-stocks'
-  }
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string }
+}) {
+  // let option = props?.searchParams.sort
+  const query = searchParams
+  const option = query?.sort || 'soaring-stocks'
   const sortResult: StockSortType[] = await sortAPI(option)
 
   let date = dateFormmating(sortResult[0]?.dateTime)
 
   return (
     <main>
-      <Headers title="주식 순위" />
+      <Headers title="급등 / 급락주" />
       <SortButton />
       <span className="ml-2 text-center">{date} 기준입니다.</span>
       <SortRank sortResult={sortResult} />
