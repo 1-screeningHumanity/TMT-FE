abeecd26c57919c182fe70efabe5ea382212acfa
jmlee119 <jmlee119@naver.com>
2024-06-18 10:45:19 +0900

    feat : 메인 페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,19 @@ import Kospi from '@/components/pages/mainpages/Kospi'
 import { KosdaqAPI, KospiAPI } from '@/actions/stock/mainpage'
 import { kospiType } from '@/types/Mainpage'
 import Kosdaq from '@/components/pages/mainpages/Kosdaq'
+import { dateFormmating } from '@/utils/time'
 
 export default async function Home() {
   const kospiData: kospiType = await KospiAPI()
   const kosdaqData: kospiType = await KosdaqAPI()
+  const date = dateFormmating(kospiData.dateTime)
 
   return (
     <div>
       <Headers title="홈" />
       <SearchBarUI />
       <span className="font-bold text-xl m-3 ">국내 시장 지표</span>
+      <div className="ml-3 my-1">{date} 기준</div>
       <div className="flex">
         <Kospi data={kospiData} />
         <Kosdaq data={kosdaqData} />
