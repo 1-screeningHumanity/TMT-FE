b6dc9f255dbe96debcc0f4221d4cee266bfc35cf
jmlee119 <jmlee119@naver.com>
2024-06-18 09:38:00 +0900

    fix : build 에러

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import AuthProvider from "../components/provider/AuthProvider";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import AuthProvider from '../components/provider/AuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "티끌 모의 태산",
-  description: "모의투자 서비스 티끌 모의 태산 입니다.",
-  authors : [],
-  keywords : "모의투자, 주식, 주식투자, 주식모의투자, 주식모의투자서비스, 티끌모의태산",
-};
-
+  title: '티끌 모의 태산',
+  description: '모의투자 서비스 티끌 모의 태산 입니다.',
+  authors: [],
+  keywords:
+    '모의투자, 주식, 주식투자, 주식모의투자, 주식모의투자서비스, 티끌모의태산',
+}
 
 export default function RootLayout({
   children,
@@ -21,9 +21,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={inter.className}>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   )
