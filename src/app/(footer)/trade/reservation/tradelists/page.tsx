f14c8c5f0b3e3f627e98204beaf0915f39f98e43
sw-devr <sw4689@naver.com>
2024-06-19 09:06:46 +0900

    fix : 매수/매도 페이지 UI 수정

--- src/app/(footer)/trade/reservation/tradelists/page.tsx
+++ src/app/(footer)/trade/reservation/tradelists/page.tsx
@@ -1,5 +1,6 @@
 import { getReservationTradeList } from "@/actions/tradeList";
 import Headers from "@/components/ui/Headers";
+import ButtonOfCancleTrade from "@/components/ui/buttons/ButtonOfCancleTrade";
 import { ReservationTradeLists } from "@/lib/trade/ReservationTradeLists";
 import { parseDate } from "@/utils/parseDate";
 
@@ -14,25 +15,25 @@ export default async function reservationTradeLists(){
     <section>
       <Headers title="예약 매수/매도 내역"/>
       <div className="w-80 bg-[#7d00d0] text-white h-10 mx-auto flex items-center justify-center rounded-md mt-5">예약 내역</div>
-      <div className="flex gap-2 text-lg overflow-x-scroll text-center mt-5 border-b-2 py-2 font-[Pretendard-Regular] pl-4">
-        <span className="w-24 shrink-0">예약시각</span> 
-        <span className="w-24 shrink-0">종목명</span> 
-        <span className="w-20 shrink-0">예약금액</span> 
-        <span className="w-20 shrink-0">수량</span> 
-        <span className="w-20 shrink-0">총 금액</span> 
-        <span className="w-24 shrink-0">매수 / 매도</span> 
-        <span className="w-20 shrink-0">취소</span> 
+      <div className="flex gap-2 text-lg overflow-x-scroll text-center mt-5 border-b-2 py-2 font-[Pretendard-Regular] pl-4 w-full">
+        <span className="w-3/12 shrink-0">예약시각</span> 
+        <span className="w-4/12 shrink-0">종목명</span> 
+        <span className="w-2/12 shrink-0">예약금액</span> 
+        <span className="w-2/12 shrink-0">수량</span> 
+        <span className="w-2/12 shrink-0">총 금액</span> 
+        <span className="w-3/12 shrink-0">사기 / 팔기</span> 
+        <span className="w-3/12 shrink-0">취소</span> 
       </div>
       <div className="overflow-x-scroll mb-20">
-      {ReservationTradeLists.map((trade, index) => (
-        <div key={index} className="flex gap-2 text-lg text-center mt-2 py-2 font-[Pretendard-Regular] pl-4 pr-4">
-          <span className="w-24 shrink-0">{parseDate(trade.time)}</span>
-          <span className="w-24 shrink-0">{trade.stockName}</span>
-          <span className="w-20 shrink-0">{trade.price.toLocaleString()}</span>
-          <span className="w-20 shrink-0">{trade.amount}</span>
-          <span className="w-20 shrink-0">{trade.totalPrice.toLocaleString()}</span>
-          <span className={`w-24 shrink-0 ${trade.status == "매도" ? "text-blue-400" : "text-red-400"}`}>{trade.status}</span>
-          <button className="w-20 bg-red-500 text-white shrink-0">취소</button>
+      {reservationTradeLists.map((trade : any, index : number) => (
+        <div key={index} className="flex gap-2 text-lg text-center mt-2 py-4 font-[Pretendard-Regular] pl-4 leading-8">
+          <span className="w-3/12 shrink-0">{parseDate(trade.time)}</span>
+          <span className="w-4/12 shrink-0">{trade.stockName}</span>
+          <span className="w-2/12 shrink-0">{trade.price.toLocaleString()}</span>
+          <span className="w-2/12 shrink-0">{trade.amount}</span>
+          <span className="w-2/12 shrink-0">{trade.totalPrice.toLocaleString()}</span>
+          <span className={`w-3/12 shrink-0 ${trade.status == "SALE" ? "text-blue-400" : "text-red-400"}`}>{trade.status == "SALE" ? "사기" : "팔기"}</span>
+          <ButtonOfCancleTrade id={trade.id} status={trade.status}/>
         </div>
       ))}
       </div>
--- src/app/(footer)/trade/tradelists/page.tsx
+++ src/app/(footer)/trade/tradelists/page.tsx
@@ -15,23 +15,23 @@ export default async function tradeLists(){
     <section>
       <Headers title="매수/매도 내역"/>
       <div className="w-80 bg-[#7d00d0] text-white h-10 mx-auto flex items-center justify-center rounded-md mt-5">최근 거래</div>
-      <section className="flex gap-2 text-lg overflow-x-scroll text-center mt-5 border-b-2 py-2 font-[Pretendard-Regular] pl-4">
-        <span className="w-24 shrink-0">체결시각</span> 
-        <span className="w-24 shrink-0">종목명</span> 
-        <span className="w-20 shrink-0">체결액</span> 
-        <span className="w-20 shrink-0">수량</span> 
-        <span className="w-20 shrink-0">총 금액</span> 
-        <span className="w-24 shrink-0">매수 / 매도</span> 
+      <section className="flex gap-2 text-lg overflow-x-scroll text-center mt-5 border-b-2 py-2 font-[Pretendard-Regular] pl-4 w-full">
+        <span className="w-3/12 shrink-0">체결시각</span> 
+        <span className="w-4/12 shrink-0">종목명</span> 
+        <span className="w-2/12 shrink-0">체결액</span> 
+        <span className="w-2/12 shrink-0">수량</span> 
+        <span className="w-2/12 shrink-0">총 금액</span> 
+        <span className="w-3/12 shrink-0">사기 / 팔기</span> 
       </section>
       <div className="overflow-x-scroll mb-20">
-      {TradeLists.map((trade, index) => (
-        <div key={index} className="flex gap-2 text-lg text-center mt-2 py-2 font-[Pretendard-Regular] pl-4">
-          <span className="w-24 shrink-0">{parseDate(trade.time)}</span>
-          <span className="w-24 shrink-0">{trade.stockName}</span>
-          <span className="w-20 shrink-0">{trade.price.toLocaleString()}</span>
-          <span className="w-20 shrink-0">{trade.amount}</span>
-          <span className="w-20 shrink-0">{trade.totalPrice.toLocaleString()}</span>
-          <span className={`w-24 shrink-0 ${trade.status == "매도" ? "text-blue-400" : "text-red-400"}`}>{trade.status}</span>
+      {tradeLists.map((trade : any, index : any) => (
+        <div key={index} className="flex gap-2 text-lg text-center mt-2 py-4 font-[Pretendard-Regular] pl-4 leading-8">
+          <span className="w-3/12 shrink-0">{parseDate(trade.time)}</span>
+          <span className="w-4/12 shrink-0">{trade.stockName}</span>
+          <span className="w-2/12 shrink-0">{trade.price.toLocaleString()}</span>
+          <span className="w-2/12 shrink-0">{trade.amount}</span>
+          <span className="w-2/12 shrink-0">{trade.totalPrice.toLocaleString()}</span>
+          <span className={`w-3/12 shrink-0 ${trade.status == "SALE" ? "text-blue-400" : "text-red-400"}`}>{trade.status == "SALE" ? "사기" : "팔기"}</span>
         </div>
       ))}
       </div>
