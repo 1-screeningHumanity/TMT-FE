4514bcc25c2b168faaf59fcee4d8a5896165bfb3
sw-devr <sw4689@naver.com>
2024-06-21 14:56:17 +0900

    fix : 간단한 코드 수정

--- src/app/(footer)/mypage/trade/reservation/tradelists/page.tsx
+++ src/app/(footer)/mypage/trade/reservation/tradelists/page.tsx
@@ -2,14 +2,13 @@ import { getReservationTradeList } from "@/actions/tradeList";
 import Headers from "@/components/ui/Headers";
 import ButtonOfCancleTrade from "@/components/ui/buttons/ButtonOfCancleTrade";
 import { ReservationTradeLists } from "@/lib/trade/ReservationTradeLists";
+import { ReservationTradeListsType } from "@/types/ReservationTradeListsType";
 import { parseDate } from "@/utils/parseDate";
 
 export default async function reservationTradeLists(){
   
   const data = await getReservationTradeList();
-  console.log("data :", data);
   const reservationTradeLists = data?.data;
-  console.log("reservationTradeLists :", reservationTradeLists);
   
   return (
     <section>
@@ -25,8 +24,8 @@ export default async function reservationTradeLists(){
         <span className="w-3/12 shrink-0">취소</span> 
       </div>
       <div className="overflow-x-scroll mb-20">
-      {reservationTradeLists.map((trade : any, index : number) => (
-        <div key={index} className="flex gap-2 text-lg text-center mt-2 py-4 font-[Pretendard-Regular] pl-4 leading-8">
+      {reservationTradeLists.map((trade : ReservationTradeListsType ) => (
+        <div key={trade.id} className="flex gap-2 text-lg text-center mt-2 py-4 font-[Pretendard-Regular] pl-4 leading-8">
           <span className="w-3/12 shrink-0">{parseDate(trade.time)}</span>
           <span className="w-4/12 shrink-0">{trade.stockName}</span>
           <span className="w-2/12 shrink-0">{trade.price.toLocaleString()}</span>
