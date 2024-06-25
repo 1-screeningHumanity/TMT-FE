2b73972c684f786dde4404f578d1299c9149d264
sw-devr <sw4689@naver.com>
2024-06-25 22:00:08 +0900

    fix : footer 수정

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,20 +1,52 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
-import { footer } from '@/lib/footer'
+import { footerSignedIn, footerSignedOut } from '@/lib/footer'
+import Navi01 from './icons/Navi01'
+import Navi02 from './icons/Navi02'
+import Navi03 from './icons/Navi03'
+import Navi04 from './icons/Navi04'
+import Navi05 from './icons/Navi05'
+import { useSession } from 'next-auth/react'
+import { useEffect, useState } from 'react'
+import { footerType } from '@/types/footerType'
 
 export default function Footer() {
+
+  const [footer, setFooter] = useState<footerType[]>();
+  const session = useSession()
+  console.log("session : ", session?.data?.user?.isSuccess);
+  const isSignIn = session?.data?.user?.isSuccess
+
+  useEffect(() => {
+    if (isSignIn) {
+      setFooter(footerSignedIn);
+    } else {
+      setFooter(footerSignedOut);
+    }
+  }, [isSignIn])
+
+
   return (
-    <div className="fixed bottom-0  w-full flex justify-around bg-white pt-2 z-10">
-      {footer.map((data) => (
-        <Link
-          href={data.href}
-          className="text-xs text-slate-400 flex items-center flex-col justify-center gap-2 w-16"
-          key={data.id}
-        >
-          <Image width="30" height="30" src={data.src} alt={data.alt} />
-          {data.name}
-        </Link>
-      ))}
-    </div>
+    <footer className="fixed bottom-0  w-full bg-white p-4 z-10">
+      <ul className='flex justify-between items-center'>
+        {footer?.map((data : footerType) => (
+          <li key={data.id}>
+            <Link
+              href={data.href}
+              className="text-xs flex items-center flex-col justify-center gap-2 w-16"
+            >
+              {data.id === 1 && <Navi01 />}
+              {data.id === 2 && <Navi02 />}
+              {data.id === 3 && <Navi03 />}
+              {data.id === 4 && <Navi04 />}
+              {data.id === 5 && <Navi05 />}
+              {data.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </footer>
   )
 }
