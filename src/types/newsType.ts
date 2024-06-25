609a442d15473aad1d206afebd65e0c650f514b0
jmlee119 <jmlee119@naver.com>
2024-06-25 16:51:24 +0900

    feat : 뉴스 데이터 타입

---
+++ src/types/newsType.ts
@@ -0,0 +1,11 @@
+interface newsType {
+  id: number
+  title: string
+  description: string
+  originallink: string
+  link: string
+  pubDate: string
+  image: string
+}
+
+export type { newsType }
