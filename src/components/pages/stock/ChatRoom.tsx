824d085d98b7f1ec4683ff2f1bb054f0055f1730
jmlee119 <jmlee119@naver.com>
2024-06-25 10:27:55 +0900

    feat : 채팅방 수정

--- src/components/pages/stock/ChatRoom.tsx
+++ src/components/pages/stock/ChatRoom.tsx
@@ -2,51 +2,95 @@
 import { userInformation } from '@/actions/myPage'
 import { chatMessage } from '@/lib/chatmessage'
 import { beforeMinute } from '@/utils/formattinfTime'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 
 export default function ChatRoom(stockCode: { stockCode: string }) {
-  const message = chatMessage
   const StockCode = stockCode.stockCode
 
   const [messageData, setMessageData] = useState<ChatMessageDataType[]>([])
+  const lastMessageRef = useRef<HTMLDivElement>(null)
+
+  const [lastId, setLastId] = useState<string>(' ')
+  const chatRoomRef = useRef<HTMLDivElement>(null)
+  // console.log(messageData)
+  // const observeIntersection = useCallback(() => {
+  //   if (chatRoomRef.current) {
+  //     const observer = new IntersectionObserver((entries) => {
+  //       entries.forEach((entry) => {
+  //         if (entry.isIntersecting && messageData[0] != undefined) {
+  //           console.log('intersected')
+  //           setLastId(messageData[0].id)
+  //         }
+  //       })
+  //     })
+  //     observer.observe(chatRoomRef.current)
+  //     return () => observer.disconnect()
+  //   }
+  // }, [chatRoomRef])
+  // console.log(lastId)
+
+  useEffect(() => {
+    if (lastMessageRef.current) {
+      lastMessageRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+    // if (inView) {
+    //   console.log(inView)
+    //   if (messageData[0] != undefined) {
+    //     setLastId(messageData[0].id)
+    //   }
+    // }
+  }, [messageData])
+  // useEffect(() => {
+  //   observeIntersection()
+  // }, [observeIntersection])
+
   useEffect(() => {
     if (!window.EventSource) {
       console.error('EventSource is not supported in this environment.')
       return
     }
-    const connetToSSE = () => {
+
+    const connectToSSE = () => {
       const eventSource = new EventSource(
-        `${process.env.API_BASE_URL}/stockitem/chat/${StockCode}`,
+        `${process.env.API_BASE_URL}/stockitem/chat/${StockCode}?lastId=${lastId}`,
+        // https://screeninghumanity.shop/api/v1/stockitem/chat/005830?lastId=6678fc576553d06892097977
       )
 
       eventSource.onmessage = (event) => {
         const data = JSON.parse(event.data)
         setMessageData((prevMessages) => {
-          const isDuplicate = prevMessages.some((msg) => msg.id == data.id)
+          const isDuplicate = prevMessages.some((msg) => msg.id === data.id)
           if (!isDuplicate) {
             return [...prevMessages, data]
           }
-
           return prevMessages
         })
       }
+
       eventSource.onerror = (error) => {
-        console.log(error)
+        console.error('EventSource failed:', error)
+        eventSource.close()
       }
+
       return eventSource
     }
-    const eventSource = connetToSSE()
+
+    const eventSource = connectToSSE()
     return () => {
       eventSource.close()
     }
-  }, [StockCode])
+  }, [StockCode, lastId]) // Include lastId in dependency array
 
   return (
-    <section className="flex flex-col mb-32">
-      {messageData.map((msg) =>
-        // 백엔드에서 현재 닉네임이 아닌 uuid 만 나옴 곧 닉네임 나오게 수정하신다고 합니다. 일단 uuid 랑 본인 같으면 오른쪽으로 나오게 수정했습니다.
-        msg.sender != 'c583a6d3-bcc8-4e00-a0ca-466be206fffe' ? (
-          <div key={msg.id} className="speech-bubble">
+    <section className="flex flex-col mb-32" ref={chatRoomRef}>
+      {messageData.map((msg, index) => {
+        return msg.sender !== 'c583a6d3-bcc8-4e00-a0ca-466be206fffe' ? (
+          <div
+            key={msg.id}
+            className="speech-bubble"
+            ref={index === messageData.length - 1 ? lastMessageRef : null}
+          >
             <div className="font-semibold">{msg.sender}</div>
             <div>{msg.message}</div>
             <div className="relative p-2">
@@ -56,7 +100,11 @@ export default function ChatRoom(stockCode: { stockCode: string }) {
             </div>
           </div>
         ) : (
-          <div key={msg.id} className="tospeech-bubble">
+          <div
+            key={msg.id}
+            className="tospeech-bubble"
+            ref={index === messageData.length - 1 ? lastMessageRef : null}
+          >
             <div className="font-semibold">{msg.sender}</div>
             <div>{msg.message}</div>
             <div className="relative p-2">
@@ -65,8 +113,8 @@ export default function ChatRoom(stockCode: { stockCode: string }) {
               </span>
             </div>
           </div>
-        ),
-      )}
+        )
+      })}
     </section>
   )
 }
