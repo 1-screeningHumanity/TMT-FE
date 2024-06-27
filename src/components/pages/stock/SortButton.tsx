408f1150024a5c21556f571c5c70e401d75bd763
sw-devr <sw4689@naver.com>
2024-06-27 14:14:51 +0900

    fix : 간단한 UI 수정

--- src/components/pages/stock/SortButton.tsx
+++ src/components/pages/stock/SortButton.tsx
@@ -1,19 +1,19 @@
 import Link from 'next/link'
 export default function SortButton() {
   return (
-    <div className="flex mx-5 my-6 justify-between">
+    <div className="flex mx-5 my-6 justify-around">
       <Link
         href={{ pathname: '/stockRank', query: { sort: 'soaring-stocks' } }}
-        className="w-5/12 h-14 font-bold flex text-white rounded-2xl items-center text-center justify-center"
-        style={{ backgroundColor: '#ff0000' }}
+        className="w-5/12 h-12 font-semibold flex text-white rounded-lg items-center text-center justify-center bg-rose-500"
+        // style={{ backgroundColor: '#ff0000' }}
         // onClick={() => router.push('/stockRank?sort=soaring-stocks')}
       >
         급등률
       </Link>
       <Link
         href={{ pathname: '/stockRank', query: { sort: 'plummeting-stocks' } }}
-        className="w-5/12 h-14 font-bold flex text-white rounded-2xl items-center text-center justify-center"
-        style={{ backgroundColor: '#0000ff' }}
+        className="w-5/12 h-12 font-semibold flex text-white rounded-lg items-center text-center justify-center bg-sky-500"
+        // style={{ backgroundColor: '#0000ff' }}
         // onClick={() => router.push('/stockRank?sort=plummeting-stocks')}
       >
         급락률
--- src/components/ui/SubscribeBox.tsx
+++ src/components/ui/SubscribeBox.tsx
@@ -1,4 +1,5 @@
 import { subScribeListType } from "@/types/subScribeListType";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function SubscribeBox(props : subScribeListType){
@@ -7,13 +8,15 @@ export default function SubscribeBox(props : subScribeListType){
 
   return (
     <div className="w-5/6 bg-slate-200 mx-auto mt-5 h-20 rounded-md flex items-center shadow-md">
-    <div className="rounded-full bg-slate-300 w-14 h-14 ml-4"></div>
+    <div className="rounded-full bg-slate-300 w-14 h-14 ml-4 flex items-center justify-center">
+      <Image src="/assets/images/logo3.svg" width={40} height={40} alt="profile" className=""/>
+    </div>
     <div className="ml-10 text-center">
-      <div className="flex justify-between ml-10">
+      <div className="flex justify-around ml-4 gap-4">
         <span>{nickName}</span>
         <span>{rank}위</span>
       </div>
-      <Link href={`/profile/${nickName}`} className="bg-white w-52 rounded-md shadow-md ml-2 mt-2">포트폴리오 보기</Link>
+      <Link href={`/profile/${nickName}`} className="bg-white w-40 rounded-md shadow-md ml-2 mt-2 inline-block">포트폴리오 보기</Link>
     </div>
   </div>
   )
