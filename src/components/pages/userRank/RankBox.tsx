eb95308a33d35e8d0e6c6d69d1d7744f062db368
sw-devr <sw4689@naver.com>
2024-06-27 14:19:13 +0900

    feat : 랭킹페이지 컴포넌트 분리

--- src/components/pages/userRank/RankBox.tsx
+++ src/components/pages/userRank/RankBox.tsx
@@ -1,17 +1,19 @@
 import { rankBoxType } from "@/types/rankBoxType";
+import formatNumberWithCommas from "@/utils/formatNumberWithCommas";
+import { ArrowDownIcon, ArrowUpIcon, MinusIcon } from "lucide-react";
 import Link from "next/link";
 
 
 
 
-export default function RankBox({ rank, changingRank, nickname, profit } : rankBoxType ) {
+export default function RankBox({ rank, changingRank, nickname, profit, isAsseted } : rankBoxType ) {
 
   return(
     <Link href={`/profile/${nickname}`} className="flex justify-around border-[1px] text-lg h-14 bg-slate-50 items-center mt-2">
-      <div>{rank}위</div>
-      <div className={`${changingRank > 0 ? "text-red-400" : "text-blue-400"}`}>{changingRank > 0 ? "▲" : "▼"} {changingRank}</div>
-      <div>{nickname}</div>
-      <div>{profit} %</div>
+      <div className="w-1/4 text-center">{rank}위</div>
+      <div className={`flex items-center justify-center w-1/4 ${changingRank > 0 ? "text-red-400" : (changingRank === 0 ? "text-green-400" :"text-blue-400")}`}>{changingRank > 0 ? <ArrowUpIcon size={15}/>: (changingRank === 0 ? <MinusIcon size={15}/> :<ArrowDownIcon size={15}/>)} {changingRank === 0 ? null : changingRank}</div>
+      <div className="w-1/2 text-center">{nickname}</div>
+      <div className="w-1/2 text-center">{formatNumberWithCommas(profit)} {isAsseted ? "원" : "%" }</div>
     </Link>
   )
 }
--- src/components/pages/userRank/RankHeader.tsx
+++ src/components/pages/userRank/RankHeader.tsx
@@ -1,10 +1,10 @@
-export default function RankHeader(){
+export default function RankHeader({columns} : {columns: string[]}){
   return(
     <div className="flex justify-around border-b-[1px] border-[#c6c6c7] mt-10 text-lg">
-      <span>순위</span>
-      <span>변동</span>
-      <span>닉네임</span>
-      <span>수익률</span>
+      <span className="w-1/4 text-center">{columns[0]}</span>
+      <span className="w-1/4 text-center">{columns[1]}</span>
+      <span className="w-1/2 text-center">{columns[2]}</span>
+      <span className="w-1/2 text-center">{columns[3]}</span>
     </div>
   )
 }
