b178c12b8810377f0b0395bc306beb8849e96859
sw-devr <sw4689@naver.com>
2024-06-27 13:57:29 +0900

    feat : 랭킹관련 api 추가

---
+++ src/actions/userRank.ts
@@ -0,0 +1,48 @@
+import { GetAPI } from "./fetchAPI";
+import { getAccessToken } from "./tokens";
+
+async function getAssetRank(){
+  const res = await GetAPI("/rank/asset");
+  return res
+}
+
+async function getMyAssetRank(){
+  const token = await getAccessToken();
+  const res = await GetAPI("/rank/my-asset", undefined, token);
+  return res
+}
+
+async function getDailyRevenueRank(){
+  const res = await GetAPI("/rank/revenue");
+  return res
+}
+
+async function getMyDailyRevenueRank(){
+  const token = await getAccessToken();
+  const res = await GetAPI("/rank/my-revenue", undefined, token);
+  return res
+}
+
+async function getWeeklyRevenueRank(){
+  const res = await GetAPI("/rank/weekly/revenue");
+  return res
+}
+
+async function getMyWeeklyRevenueRank(){
+  const token = await getAccessToken();
+  const res = await GetAPI("/rank/weekly/my-revenue", undefined, token);
+  return res
+}
+
+async function getMonthlyRevenueRank(){
+  const res = await GetAPI("/rank/monthly/revenue");
+  return res
+}
+
+async function getMyMonthlyRevenueRank(){
+  const token = await getAccessToken();
+  const res = await GetAPI("/rank/monthly/my-revenue", undefined, token);
+  return res
+}
+
+export { getAssetRank, getMyAssetRank, getDailyRevenueRank, getMyDailyRevenueRank, getWeeklyRevenueRank, getMyWeeklyRevenueRank, getMonthlyRevenueRank, getMyMonthlyRevenueRank }
