a048197ab68388b526beb4bb76470b7b948cde70
jmlee119 <jmlee119@naver.com>
2024-06-28 15:26:46 +0900

    feat : 줄바꿈해결

--- src/components/pages/stock/FromChat.tsx
+++ src/components/pages/stock/FromChat.tsx
@@ -5,7 +5,7 @@ export default function FromChat({ msg }: { msg: ChatMessageDataType }) {
   return (
     <div key={msg.id} className="speech-bubble">
       <div className="font-semibold">{msg.nickName}</div>
-      <div>{msg.message}</div>
+      <pre className=" w-full whitespace-pre-wrap">{msg.message}</pre>
       <div className="relative p-2">
         <span className="text-xs absolute right-0 bottom-0 pb-1 mr-2">
           {beforeMinute(msg.createAt)}
