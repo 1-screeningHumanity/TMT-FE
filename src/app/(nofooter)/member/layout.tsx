5154c21d770f230383984dd96e4295591fe63a73
jmlee119 <jmlee119@naver.com>
2024-06-29 17:17:02 +0900

    feat : 로그인 안햇을때 x 표시로 만들기

---
+++ src/app/(nofooter)/member/layout.tsx
@@ -0,0 +1,28 @@
+import Logo from '@/components/layouts/Logo'
+import SubHeader from '@/components/layouts/SubHeader'
+import Link from 'next/link'
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <section className="fixed z-[1030] w-full h-[100vh] overflow-y-scroll">
+      <nav className="w-full fixed top-0 z-50">
+        <ul
+          className="p-6 flex justify-between items-center bg-slate-100"
+          style={{
+            backdropFilter: 'blur(10px)',
+            WebkitBackdropFilter: 'blur(10px)',
+          }}
+        >
+          <Link href={'/'}>
+            <Logo />
+          </Link>
+        </ul>
+      </nav>
+      {children}
+    </section>
+  )
+}
--- src/app/(nofooter)/member/signin/page.tsx
+++ src/app/(nofooter)/member/signin/page.tsx
@@ -1,29 +1,47 @@
-import SigninForm from "@/components/forms/SigninForm";
-import ButtonOfSigninFromKakao from "@/components/ui/buttons/ButtonOfSigninFromKakao";
-import ButtonOfSigninFromNaver from "@/components/ui/buttons/ButtonOfSigninFromNaver";
-import { Toaster } from "@/components/ui/toaster";
-import Image from "next/image";
-import Link from "next/link";
+import SigninForm from '@/components/forms/SigninForm'
+import ButtonOfSigninFromKakao from '@/components/ui/buttons/ButtonOfSigninFromKakao'
+import ButtonOfSigninFromNaver from '@/components/ui/buttons/ButtonOfSigninFromNaver'
+import { Toaster } from '@/components/ui/toaster'
+import Image from 'next/image'
+import Link from 'next/link'
 
-export default function signin(){
-
-
-  return(
+export default function signin() {
+  return (
     <section className="w-full mx-auto px-10">
-      <Image src={"/assets/images/logo2.svg"} alt="logo2" width={100} height={100} className="mx-auto mt-20 mb-16"/>
+      <Image
+        src={'/assets/images/logo2.svg'}
+        alt="logo2"
+        width={100}
+        height={100}
+        className="mx-auto mt-20 mb-16"
+      />
       <SigninForm />
-      <p className="text-xs mx-auto w-fit text-[#775da6]">비밀번호가 기억이 나지 않나요? <Link href={"/member/reset/password"} className="font-extrabold">비밀번호 찾기</Link></p>
+      <p className="text-xs mx-auto w-fit text-[#775da6]">
+        비밀번호가 기억이 나지 않나요?{' '}
+        <Link href={'#'} className="font-extrabold">
+          비밀번호 찾기
+        </Link>
+      </p>
       <div className="relative">
-        <hr className="w-full my-6"/>
-        <p className="text-xs w-fit text-center bg-slate-100 px-4 py-2 mx-auto absolute top-[-15px] left-[50%] translate-x-[-50%]">간단 로그인</p>
+        <hr className="w-full my-6" />
+        <p className="text-xs w-fit text-center bg-slate-100 px-4 py-2 mx-auto absolute top-[-15px] left-[50%] translate-x-[-50%]">
+          간단 로그인
+        </p>
       </div>
       <div className="flex justify-between w-full">
         <ButtonOfSigninFromNaver />
         <ButtonOfSigninFromKakao />
       </div>
-      <Link href={"/member/signup"} className="border-[1px] w-full h-10 mx-auto my-6 block rounded-lg text-[#7d00d0] border-[#7d00d0] font-extrabold text-sm text-center leading-10">계정만들기</Link>
-      <p className="text-xs text-center mt-16 text-[#8e9195]">©2024 TMINVESTMENT.com<br/>
-          ALL RIGHTS RESERVED.
+      <Link
+        href={'/member/signup'}
+        className="border-[1px] w-full h-10 mx-auto my-6 block rounded-lg text-[#7d00d0] border-[#7d00d0] font-extrabold text-sm text-center leading-10"
+      >
+        계정만들기
+      </Link>
+      <p className="text-xs text-center mt-16 text-[#8e9195]">
+        ©2024 TMINVESTMENT.com
+        <br />
+        ALL RIGHTS RESERVED.
       </p>
     </section>
   )
