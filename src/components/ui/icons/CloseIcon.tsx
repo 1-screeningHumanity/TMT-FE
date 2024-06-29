6022724f2c2b75b19cba556c111331a858c8f1f9
jmlee119 <jmlee119@naver.com>
2024-06-29 16:26:36 +0900

    feat : svg 아이콘 수정

--- src/components/ui/icons/CloseIcon.tsx
+++ src/components/ui/icons/CloseIcon.tsx
@@ -2,37 +2,32 @@ import React from 'react'
 
 function CloseIcon() {
   return (
-    <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g filter="url(#filter0_d_109_3043)">
-    <path d="M7.02783 7.40039L20.2272 20.5997" stroke="#142535" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    </g>
-    <g filter="url(#filter1_d_109_3043)">
-    <path d="M7.02783 20.5996L20.2272 7.40028" stroke="#142535" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    </g>
-    <defs>
-    <filter id="filter0_d_109_3043" x="4.02783" y="4.40039" width="23.1992" height="23.1992" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
-    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-    <feOffset dx="2" dy="2"/>
-    <feGaussianBlur stdDeviation="2"/>
-    <feComposite in2="hardAlpha" operator="out"/>
-    <feColorMatrix type="matrix" values="0 0 0 0 0.972549 0 0 0 0 0.298039 0 0 0 0 0.419608 0 0 0 1 0"/>
-    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_109_3043"/>
-    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_109_3043" result="shape"/>
-    </filter>
-    <filter id="filter1_d_109_3043" x="4.02783" y="4.40039" width="23.1992" height="23.1992" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
-    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-    <feOffset dx="2" dy="2"/>
-    <feGaussianBlur stdDeviation="2"/>
-    <feComposite in2="hardAlpha" operator="out"/>
-    <feColorMatrix type="matrix" values="0 0 0 0 0.972549 0 0 0 0 0.298039 0 0 0 0 0.419608 0 0 0 1 0"/>
-    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_109_3043"/>
-    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_109_3043" result="shape"/>
-    </filter>
-    </defs>
+    <svg
+      width="28"
+      height="28"
+      viewBox="0 0 28 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d_109_3043)">
+        <path
+          d="M7.02783 7.40039L20.2272 20.5997"
+          stroke="#142535"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <g filter="url(#filter1_d_109_3043)">
+        <path
+          d="M7.02783 20.5996L20.2272 7.40028"
+          stroke="#142535"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
     </svg>
-
   )
 }
 
