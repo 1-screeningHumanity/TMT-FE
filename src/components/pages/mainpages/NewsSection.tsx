ec04acd376014220c861802b2b27b74701d77713
jmlee119 <jmlee119@naver.com>
2024-06-29 16:10:31 +0900

    feat : 이미지 null 일 경우 처리

--- src/components/pages/mainpages/NewsSection.tsx
+++ src/components/pages/mainpages/NewsSection.tsx
@@ -1,14 +1,16 @@
 'use client'
 import { newsType } from '@/types/newsType'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Autoplay } from 'swiper/modules'
-import { useState, useEffect } from 'react'
+import { Navigation, Scrollbar, Autoplay } from 'swiper/modules'
+import SwiperCore from 'swiper'
+import { useEffect, useState } from 'react'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 
 export default function NewsSection(newsData: { newsData: newsType[] }) {
   const data = newsData.newsData
+  const bgWhite = '/assets/images/bgWhite.svg'
 
   return (
     <Swiper
@@ -20,22 +22,26 @@ export default function NewsSection(newsData: { newsData: newsType[] }) {
       modules={[Navigation, Autoplay]}
       autoplay={{
         delay: 2000,
-        disableOnInteraction: false,
+        disableOnInteraction: false, // 사용자 상호작용시 슬라이더 일시 정지 비활성
       }}
     >
       {data.map((news) => (
         <SwiperSlide key={news.id}>
-          <a href={news.originallink} className="m-4">
-            <section className="relative flex flex-col bg-slate-50 overflow-hidden w-1/3 h-auto">
-              <div className="w-full aspect-w-1 aspect-h-1">
-                <img
-                  src={news.image}
-                  alt={news.description}
-                  className="object-cover w-full h-full"
-                />
-              </div>
+          <a
+            href={news.originallink}
+            className="m-4 overflow-hidden 1/3 h-auto"
+          >
+            <section className=" flex flex-col  bg-slate-50 justify-center items-center ">
+              <img
+                src={news.image || bgWhite} //  null 이 올수도 있음
+                alt={news.description}
+                className="object-cover"
+              />
               <div className="absolute bottom-0 bg-black bg-opacity-50 text-white p-2 w-full text-center">
-                <div dangerouslySetInnerHTML={{ __html: news.title }}></div>
+                <span
+                  dangerouslySetInnerHTML={{ __html: news.title }}
+                  className="line-clamp-2"
+                ></span>
               </div>
             </section>
           </a>
