f36f66b4d3dcd4ef3422b011e944dab80ddf91f3
jmlee119 <jmlee119@naver.com>
2024-07-01 20:40:31 +0900

    feat : 모달 수정

--- src/components/pages/stock/Trade.tsx
+++ src/components/pages/stock/Trade.tsx
@@ -6,7 +6,8 @@ import { toast } from '@/components/ui/use-toast'
 import { staticStockType } from '@/types/Stock'
 import { socketStockCode } from '@/utils/socketStockCode'
 import timeCheck from '@/utils/timeCheck'
-import React, { useEffect, useState } from 'react'
+import React, { use, useEffect } from 'react'
+import { useState } from 'react'
 
 export default function Trade({
   stockCode,
@@ -21,65 +22,59 @@ export default function Trade({
   const [socketFlag, setSocketFlag] = useState<boolean>(false)
   const [myMoney, setMyMoney] = useState<number>(0)
   const check = timeCheck()
-
   useEffect(() => {
     if (socketStockCode.includes(stockCode) && check === true) {
       setSocketFlag(true)
     }
     wonInfo()
   }, [])
-
   const wonInfo = async () => {
     const token = await getAccessToken()
 
-    if (token !== 'Bearer undefined') {
+    if (token != 'Bearer undefined') {
       const res = await wonInfoAPI()
       setMyMoney(res.data.won as number)
     }
   }
-
   const LoginCheck = async () => {
     const token = await getAccessToken()
-
     if (token === 'Bearer undefined') {
       toast({
         title: '로그인이 필요합니다.',
         variant: 'destructive',
       })
+      return
     } else {
       setModalOpen(true)
     }
   }
-
   return (
     <section className="Trade">
-      <div className="fixed bottom-0 left-0 right-0 h-20 bg-white justify-center">
-        <div className="mt-5 mx-5 flex justify-between ">
-          <button
-            className="w-5/12 h-14 font-bold text-white rounded-2xl"
-            style={{ backgroundColor: '#0B0B0B' }}
-            onClick={() => LoginCheck()}
-          >
-            팔기
-          </button>
-          <button
-            className="w-5/12 h-14 font-bold text-white rounded-2xl"
-            style={{ backgroundColor: '#7D12FF' }}
-            onClick={() => LoginCheck()}
-          >
-            사기
-          </button>
-        </div>
-
-        <TradeModal
-          modalOpen={modalOpen}
-          setModalOpen={setModalOpen}
-          stockCode={stockCode}
-          stockNameResult={stockName}
-          staticStockPrice={staticStockPrice}
-          myMoney={myMoney}
-        />
+      <div className="fixed bottom-0 left-0 right-0 h-20 bg-white justify-between text-center">
+        <button
+          className="w-5/12 h-14 font-bold text-white rounded-2xl mx-3"
+          style={{ backgroundColor: '#0B0B0B' }}
+          onClick={() => LoginCheck()}
+        >
+          팔기
+        </button>
+        <button
+          className="w-5/12 h-14 font-bold text-white rounded-2xl mx-3"
+          style={{ backgroundColor: '#7D12FF' }}
+          onClick={() => LoginCheck()}
+        >
+          사기
+        </button>
       </div>
+
+      <TradeModal
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+        stockCode={stockCode}
+        stockNameResult={stockName}
+        staticStockPrice={staticStockPrice}
+        myMoney={myMoney}
+      />
     </section>
   )
 }
--- src/components/pages/stock/TradeModal.tsx
+++ src/components/pages/stock/TradeModal.tsx
@@ -35,7 +35,7 @@ export default function TradeModal({
     <AnimatePresence>
       {modalOpen && (
         <motion.div
-          className="bg-black/60 fixed inset-0 z-[1000] flex items-end"
+          className="bg-black/60 fixed inset-0 z-[100] flex items-end"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
