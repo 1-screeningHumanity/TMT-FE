c92d1e09aab932d0774980bc4994e86289cae741
sw-devr <sw4689@naver.com>
2024-07-01 11:07:33 +0900

    fix : 충전하기 아이템 버튼 UI 수정

--- src/components/pages/payment/WonCardItem.tsx
+++ src/components/pages/payment/WonCardItem.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 function Card({cash, won, onClick}:{cash:number, won?:number, onClick?:()=>void}) {
   return (
     <div className="border-[1px] border-[#d9d9d9] w-80 h-20 flex items-center justify-center flex-col gap-2 hover:bg-slate-200 bg-[#edf1fc] rounded-md" onClick={onClick}>
-      <p className='text-2xl'>{formatNumberWithCommas(won)}원</p>
-      { won && <p className='text-sm'>{formatNumberWithCommas(cash)}캐시</p> }
+      <p className='text-xl'>{formatNumberWithCommas(won)}원</p>
+      { won && <p className='text-sm text-red-800'>{formatNumberWithCommas(cash)}캐시</p> }
       </div>
   )
 }
