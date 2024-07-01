1a1c7b07f0e628dc51186fd45d98d5f917f92449
sw-devr <sw4689@naver.com>
2024-07-01 13:03:07 +0900

    fix : page 타이틀 UI 수정

--- src/components/ui/TitleOfPages.tsx
+++ src/components/ui/TitleOfPages.tsx
@@ -1,7 +1,10 @@
-export default function TitleOfPages({title} : {title : string}) {
+export default async function TitleOfPages({title} : {title : string}) {
+
   return (
     <>
+    <div className="flex justify-center gap-2 items-center">
       <h1 className='text-center text-xl text-[#7d12ff] font-extrabold tracking-tighter'>{title}</h1>
+    </div>
     </>
   )
 }
