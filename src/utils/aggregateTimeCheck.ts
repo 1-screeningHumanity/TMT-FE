07a1e5f316445444e4e69612798597c7706c5296
sw-devr <sw4689@naver.com>
2024-07-01 13:05:43 +0900

    feat : 랭킹 집계시간 변경으로 인한 로직 수정

--- src/utils/aggregateTimeCheck.ts
+++ src/utils/aggregateTimeCheck.ts
@@ -6,10 +6,10 @@ function dailyAggregateTimeRange() {
   let hours = currentTime.getHours()
   let minutes = currentTime.getMinutes()
 
-  const startHour = 16
-  const startMinute = 0
-  const endHour = 17
-  const endMinute = 30
+  const startHour = 15
+  const startMinute = 40
+  const endHour = 16
+  const endMinute = 40
 
   // 현재 시간을 분 단위로 변환
   let currentTotalMinutes = hours * 60 + minutes
@@ -42,44 +42,35 @@ function isWithinFridayWindow() {
   // 현재 시간을 분으로 변환
   let currentMinutes = now.getHours() * 60 + now.getMinutes();
 
-  // 금요일 오후 4시(16:00)의 시작 시간과 오후 5시 30분(17:30)의 종료 시간
-  let startMinutes = 16 * 60;
-  let endMinutes = 17 * 60 + 30;
+  // 금요일 오후 3시 30분(15:30)의 시작 시간과 오후 4시 30분(16:30)의 종료 시간
+  let startMinutes = 15 * 60 + 40;
+  let endMinutes = 16 * 60 + 40;
 
-  // 현재 시간이 금요일 오후 4시부터 5시 30분 사이인지 확인
+  // 현재 시간이 금요일 오후 3시 30분부터 4시 30분 사이인지 확인
   return currentMinutes >= startMinutes && currentMinutes <= endMinutes;
 }
 
-function getLastFridayOfMonth(year : number, month : number) {
+function getLastDayOfMonth(year : number, month : number) {
   // 해당 달의 마지막 날
-  let lastDay = new Date(year, month + 1, 0);
-
-  // 마지막 날의 요일 (0: 일요일, 1: 월요일, ..., 6: 토요일)
-  let lastDayOfWeek = lastDay.getDay();
-
-  // 마지막 금요일로 이동
-  let daysToLastFriday = (lastDayOfWeek + 2) % 7;
-  lastDay.setDate(lastDay.getDate() - daysToLastFriday);
-
-  return lastDay;
+  return new Date(year, month + 1, 0);
 }
 
-function isWithinMonthlyFridayWindow() {
+function isWithinLastDayWindow() {
   // 현재 날짜와 시간 가져오기
   let now = new Date();
 
-  // 이번 달의 마지막 금요일 계산
-  let lastFriday = getLastFridayOfMonth(now.getFullYear(), now.getMonth());
+  // 이번 달의 마지막 날 계산
+  let lastDay = getLastDayOfMonth(now.getFullYear(), now.getMonth());
 
-  // 마지막 금요일 오후 4시와 오후 5시 30분 설정
-  let start = new Date(lastFriday);
-  start.setHours(16, 0, 0, 0); // 오후 4시
+  // 마지막 날 오후 3시 40분과 오후 4시 40분 설정
+  let start = new Date(lastDay);
+  start.setHours(15, 40, 0, 0); // 오후 3시 40분
 
-  let end = new Date(lastFriday);
-  end.setHours(17, 30, 0, 0); // 오후 5시 30분
+  let end = new Date(lastDay);
+  end.setHours(16, 40, 0, 0); // 오후 4시 40분
 
   // 현재 시간이 그 시간 범위에 속하는지 확인
   return now >= start && now <= end;
 }
 
-export { dailyAggregateTimeRange, isWithinFridayWindow, isWithinMonthlyFridayWindow }
+export { dailyAggregateTimeRange, isWithinFridayWindow, isWithinLastDayWindow }
