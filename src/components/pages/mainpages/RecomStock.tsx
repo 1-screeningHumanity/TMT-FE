8cfd2c7154ac84f2d1871c74cc2985736cf4a18c
jmlee119 <jmlee119@naver.com>
2024-07-02 02:51:32 +0900

    feat : 주식 추천

--- src/components/pages/mainpages/RecomStock.tsx
+++ src/components/pages/mainpages/RecomStock.tsx
@@ -1,6 +1,8 @@
 'use client'
 import mainpageData from '@/lib/mainpageData'
 import { motion } from 'framer-motion'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function RecomStock() {
   return (
@@ -8,22 +10,25 @@ export default function RecomStock() {
       initial={{ opacity: 0, translateY: '-10px' }}
       animate={{ opacity: 1, translateY: '0px' }}
       transition={{ duration: 0.3 }}
-      className="py-4 drop-shadow-sm "
     >
+      <div className="flex font-bold text-xl p-2">
+        추천 주식 top 20
+        <Image
+          src={'/assets/images/Increase.svg'}
+          alt="작은 주식"
+          width={25}
+          height={25}
+        />
+      </div>
       <section className="flex flex-wrap justify-around">
-        <div className="font-bold text-xl">추천 주식 top 20</div>
         {mainpageData.map((stock) => (
-          <div
-            key={stock.id}
-            className="flex items-center justify-between p-4 border-b bg-white  rounded-lg "
-          >
-            <div className="flex items-center">
-              <p className=" font-bold">{stock.stockName}</p>
+          <Link href={`/stock/${stock.stockCode}`} key={stock.id}>
+            <div className="flex items-center justify-between p-4 border-b bg-white rounded-lg ">
+              <div className="flex items-center">
+                <p className=" font-bold">{stock.stockName}</p>
+              </div>
             </div>
-            <div className="flex items-center">
-              <button className="text-xs text-blue-500">추천</button>
-            </div>
-          </div>
+          </Link>
         ))}
       </section>
     </motion.div>
