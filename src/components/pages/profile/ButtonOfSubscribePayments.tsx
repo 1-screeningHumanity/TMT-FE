894afb49d4cff683f6607467ebdb28dd761b338c
sw-devr <sw4689@naver.com>
2024-07-01 11:09:37 +0900

    fix : 구독 결제하기 버튼 수정

--- src/components/pages/profile/ButtonOfSubscribePayments.tsx
+++ src/components/pages/profile/ButtonOfSubscribePayments.tsx
@@ -15,30 +15,30 @@ export default function ButtonOfSubscribePayments({nickname, myNick} : { nicknam
     if(res.code == 200){
       toast({
         title: '구독이 완료되었습니다',
-        variant : "default",
-        action:(<button onClick={() => router.push(`/profile/${nickname}`)}>확인</button>)
+        variant : "success",
+        action:(<button onClick={() => {router.push(`/profile/${nickname}`); router.refresh()}}>확인</button>)
       })
     }else if(res.code == 201){
       toast({
         title: '구독이 완료되었습니다',
         description: '기존에 구독 한 회원은 과금되지 않습니다.',
-        variant : "default",
-        action:(<button onClick={() => router.push(`/profile/${nickname}`)}>확인</button>)
+        variant : "success",
+        action:(<button onClick={() => {router.push(`/profile/${nickname}`); router.refresh()}}>확인</button>)
       })
     }else{
       toast({
         title: res.message,
         variant : "destructive",
-        action:(<button onClick={() => router.push(`/profile/${nickname}`)}>확인</button>)
+        action:(<button onClick={() => {router.push(`/profile/${nickname}`); router.refresh()}}>확인</button>)
       })
     }
   }
 
   return (
-    <div className="w-full my-20 fixed bottom-[0px]">
-      <button className="w-full h-10 bg-[#7d12ff] text-white" onClick={handleSubscribe}>구독하기</button>
+    <div className="w-full my-20 flex justify-between ">
+      <button className="w-1/2 h-10 mx-2 bg-[#7d12ff] text-white rounded-lg" onClick={handleSubscribe}>구독하기</button>
 
-      <button className="w-full h-10 bg-[#7d12ff] text-white" onClick={() => router.push("/payments")}>충전하러 가기</button>
+      <button className="w-1/2 h-10 mx-2 bg-[#7d12ff] text-white rounded-lg" onClick={() => router.push("/payments")}>충전하러 가기</button>
     </div>
   )
 }
