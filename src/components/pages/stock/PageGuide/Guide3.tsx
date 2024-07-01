8d401865c752dcbd466f89071669a2f1f7954c6e
jmlee119 <jmlee119@naver.com>
2024-07-01 17:03:09 +0900

    feat : guide 3 수정

--- src/components/pages/stock/PageGuide/Guide3.tsx
+++ src/components/pages/stock/PageGuide/Guide3.tsx
@@ -1,6 +1,6 @@
 export default function Guide3() {
   return (
-    <div className="bg-white absolute top-72 left-5 w-24 h-auto p-2">
+    <div className="bg-white absolute top-1/4 right-10 w-24 h-auto p-2">
       차트를 다양한 방법을 볼 수 있어요
     </div>
   )
