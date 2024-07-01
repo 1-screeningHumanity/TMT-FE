f705c2c7305a5a3e268af05131fc2dc67db848a7
jmlee119 <jmlee119@naver.com>
2024-07-01 19:23:37 +0900

    feat :  메인페이지 수정

--- src/components/pages/mainpages/MyStatus.tsx
+++ src/components/pages/mainpages/MyStatus.tsx
@@ -1,41 +1,55 @@
 'use client'
 
-import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
-import CountUp from "react-countup";
-import { userInformation } from "@/actions/myPage";
-import { set } from "firebase/database";
-import { getMyAssetRank } from "@/actions/userRank";
+import { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
+import CountUp from 'react-countup'
+import { userInformation } from '@/actions/myPage'
+import { set } from 'firebase/database'
+import { getMyAssetRank } from '@/actions/userRank'
 
-function MyStatus({data, delay} : {data: any, delay: number}) {
-
-  const [show, setShow] = useState<boolean>(false);
-  const [nickName, setNickName] = useState<string>('');
-  const [rank, setRank] = useState<number>(0);
+function MyStatus({ data, delay }: { data: any; delay: number }) {
+  const [show, setShow] = useState<boolean>(false)
+  const [nickName, setNickName] = useState<string>('')
+  const [rank, setRank] = useState<number>(0)
 
   useEffect(() => {
     async function userInfo() {
-      const res = await userInformation();
-      setNickName(res?.data?.nickanme);
-      const myRankResponse = await getMyAssetRank();
-      setRank(myRankResponse?.data?.ranking);
+      const res = await userInformation()
+      setNickName(res?.data?.nickanme)
+      const myRankResponse = await getMyAssetRank()
+      setRank(myRankResponse?.data?.ranking)
     }
-    userInfo();
-    const timer = setTimeout(() => setShow(true), delay);
-    return () => clearTimeout(timer);
+    userInfo()
+    const timer = setTimeout(() => setShow(true), delay)
+    return () => clearTimeout(timer)
+  }, [delay])
 
-  }, [delay]);
-
-  if (!show) return null;
+  if (!show) return null
+  console.log(rank)
 
   return (
-    <motion.div initial={{ opacity: 0, translateY: '-10px' }} animate={{ opacity: 1, translateY: '0px' }} transition={{ duration: 0.3 }}>
-      <div className='w-full bg-white rounded-lg px-[1rem] py-[1rem] drop-shadow-sm'>
-        <p className='text-sm mb-2'>{nickName}님 어서오세요.</p>
-        <p className='text-2xl font-extrabold leading-tight'>자산순위<br/><CountUp start={0} end={rank} duration={1} separator=',' />위입니다.</p>
+    <motion.div
+      initial={{ opacity: 0, translateY: '-10px' }}
+      animate={{ opacity: 1, translateY: '0px' }}
+      transition={{ duration: 0.3 }}
+    >
+      <div className="w-full bg-white rounded-lg px-[1rem] py-[1rem] drop-shadow-sm">
+        <p className="text-sm mb-2">{nickName}님 어서오세요.</p>
+        {typeof rank === 'undefined' ? (
+          <p className="text-2xl font-extrabold leading-tight">
+            신규 회원입니다
+          </p>
+        ) : (
+          <p className="text-2xl font-extrabold leading-tight">
+            자산순위
+            <br />
+            <CountUp start={0} end={rank} duration={1} separator="," />
+            위입니다.
+          </p>
+        )}
       </div>
     </motion.div>
-  );
+  )
 }
 
-export default MyStatus;
+export default MyStatus
