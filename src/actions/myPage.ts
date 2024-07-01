7b7e2d7b939b877af4f143e670c9525d0cea78ec
sw-devr <sw4689@naver.com>
2024-07-01 09:48:08 +0900

    feat : 내 포트폴리오 API 추가

--- src/actions/myPage.ts
+++ src/actions/myPage.ts
@@ -1,16 +1,25 @@
 import { GetAPI } from "./fetchAPI"
 import { getAccessToken } from "./tokens"
 
-export async function userInformation(){
+async function userInformation(){
 
   const token = await getAccessToken()
   const res = await GetAPI('/member/mypage/information', undefined, token)
   return res
 }
 
-export async function userGrade(){
+async function userGrade(){
 
   const token = await getAccessToken()
   const res = await GetAPI('/member/grade', undefined, token)
   return res
 }
+
+async function myPortfolio(){
+
+  const token = await getAccessToken()
+  const res = await GetAPI('/portfolio/myportfolio', undefined, token)
+  return res
+}
+
+export{ userInformation, userGrade, myPortfolio }
