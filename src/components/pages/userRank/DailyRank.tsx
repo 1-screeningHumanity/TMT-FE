7d653a9a501f40a5e7b2b81c9564202a3af5cd2a
sw-devr <sw4689@naver.com>
2024-07-01 11:17:05 +0900

    fix : 일간랭킹 api 변경 및 tooltip -> popover 변경

--- src/components/pages/userRank/DailyRank.tsx
+++ src/components/pages/userRank/DailyRank.tsx
@@ -6,23 +6,29 @@ import { getCurruntDate, getPreviousDay4PM } from "@/utils/time";
 import { getDailyRevenueRank, getMyDailyRevenueRank } from "@/actions/userRank";
 import { dailyRankDataType } from "@/types/rankBoxType";
 import { dailyAggregateTimeRange } from "@/utils/aggregateTimeCheck";
-import ToolTipBox from "@/components/ui/ToolTipBox";
+import PopOverBox from "@/components/ui/PopOverBox";
 
 export default async function DailyRank() {
 
   const date = getPreviousDay4PM();
   const isValid = dailyAggregateTimeRange();
 
-  const myRankResponse = await getMyDailyRevenueRank();
-  const myRankInfo = myRankResponse?.data;
+  let myRankInfo = null;
+  let DailyRank = [];
 
-  const res = await getDailyRevenueRank();
-  const DailyRank = res?.data;
+  if(!isValid){
+    const myRankResponse = await getMyDailyRevenueRank();
+    myRankInfo = myRankResponse?.data;
+  
+    const res = await getDailyRevenueRank();
+    DailyRank = res?.data?.content;
+  }
+
 
   return(
     <>
       <p className="text-[#7d12ff] text-center leading-10 mt-4">{date} 기준입니다.</p>
-      <ToolTipBox text="매일 16시 ~ 17시 30분은 집계시간입니다."/>
+      <PopOverBox text="매일 15시 40분 ~ 16시 40분은 일간순위 집계시간입니다."/>
       <RankHeader columns={['순위','변동','닉네임','수익률']} /> 
       { isValid ? <div className="text-center my-20">현재는 순위 집계 시간입니다.</div> : 
         <>
