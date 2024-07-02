2a315fbaed423f34dbc71c16f560864264b8f770
jmlee119 <jmlee119@naver.com>
2024-07-02 12:14:06 +0900

    feat : 예약 매수 내역 수정

--- src/app/(footer)/mypage/trade/reservation/tradelists/page.tsx
+++ src/app/(footer)/mypage/trade/reservation/tradelists/page.tsx
@@ -8,51 +8,51 @@ import { parseDate } from '@/utils/parseDate'
 export default async function reservationTradeLists() {
   const data = await getReservationTradeList()
   const reservationTradeLists = data?.data
-
-  console.log(reservationTradeLists)
   return (
     <section>
       <div className="w-80 bg-[#7d00d0] text-white h-10 mx-auto flex items-center justify-center rounded-md mt-5">
         예약 내역
       </div>
-      <div className="flex gap-2 text-md overflow-x-scroll text-center mt-5 border-b-2 py-2 font-[Pretendard-Regular] pl-4 w-full">
-        <span className="w-3/12 shrink-0">예약시각</span>
-        <span className="w-4/12 shrink-0">종목명</span>
-        <span className="w-2/12 shrink-0">예약금액</span>
-        <span className="w-2/12 shrink-0">수량</span>
-        <span className="w-2/12 shrink-0">총 금액</span>
-        <span className="w-3/12 shrink-0">사기 / 팔기</span>
-        <span className="w-3/12 shrink-0">취소</span>
-      </div>
-      <div className="overflow-x-scroll mb-20">
-        {reservationTradeLists.length == 0 ? (
-          <div className="text-center mt-5">예약 내역이 없습니다.</div>
-        ) : (
-          reservationTradeLists.map((trade: ReservationTradeListsType) => (
-            <Link href={`/stock/${trade.stockCode}`}>
-              <div
-                key={trade.id}
-                className="flex gap-2 text-md text-center mt-2 py-4 font-[Pretendard-Regular] pl-4 leading-8"
-              >
-                <span className="w-3/12 shrink-0">{parseDate(trade.time)}</span>
-                <span className="w-4/12 shrink-0">{trade.stockName}</span>
-                <span className="w-2/12 shrink-0">
-                  {trade.price.toLocaleString()}
-                </span>
-                <span className="w-2/12 shrink-0">{trade.amount}</span>
-                <span className="w-2/12 shrink-0">
-                  {trade.totalPrice.toLocaleString()}
-                </span>
-                <span
-                  className={`w-3/12 shrink-0 ${trade.status == 'SALE' ? 'text-blue-400' : 'text-red-400'}`}
-                >
-                  {trade.status == 'BUY' ? '사기' : '팔기'}
-                </span>
-                <ButtonOfCancleTrade id={trade.id} status={trade.status} />
-              </div>
-            </Link>
-          ))
-        )}
+      <div className="overflow-x-auto">
+        <div className="flex gap-2 text-md text-center mt-5 border-b-2 py-2 font-[Pretendard-Regular] pl-4 w-max min-w-full">
+          <span className="w-3/12 shrink-0">예약시각</span>
+          <span className="w-4/12 shrink-0">종목명</span>
+          <span className="w-2/12 shrink-0">예약금액</span>
+          <span className="w-2/12 shrink-0">수량</span>
+          <span className="w-2/12 shrink-0">총 금액</span>
+          <span className="w-3/12 shrink-0">사기 / 팔기</span>
+          <span className="w-3/12 shrink-0">취소</span>
+        </div>
+        <div className="mb-20">
+          {reservationTradeLists.length == 0 ? (
+            <div className="text-center mt-5">예약 내역이 없습니다.</div>
+          ) : (
+            reservationTradeLists.map((trade: ReservationTradeListsType) => (
+              <Link href={`/stock/${trade.stockCode}`} key={trade.id}>
+                <div className="flex gap-2 text-md text-center mt-2 py-2 font-[Pretendard-Regular] pl-4 leading-8 w-max min-w-full">
+                  <span className="w-3/12 shrink-0">
+                    {parseDate(trade.time)}
+                  </span>
+                  <span className="w-4/12 shrink-0">{trade.stockName}</span>
+                  <span className="w-2/12 shrink-0">
+                    {trade.price.toLocaleString()}
+                  </span>
+                  <span className="w-2/12 shrink-0">{trade.amount}</span>
+                  <span className="w-2/12 shrink-0">
+                    {trade.totalPrice.toLocaleString()}
+                  </span>
+                  <span
+                    className={`w-3/12 shrink-0 ${trade.status == 'SALE' ? 'text-blue-400' : 'text-red-400'}`}
+                  >
+                    {trade.status == 'BUY' ? '팔기' : '사기'}
+                  </span>
+
+                  <ButtonOfCancleTrade id={trade.id} status={trade.status} />
+                </div>
+              </Link>
+            ))
+          )}
+        </div>
       </div>
     </section>
   )
