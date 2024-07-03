1da8078ea7efe3c9e631fc79aa1a6af15bb8d7a4
sw-devr <sw4689@naver.com>
2024-07-03 10:48:37 +0900

    feat : 일간랭킹페이지 무한스크롤 추가

--- src/components/pages/userRank/WeeklyRank.tsx
+++ src/components/pages/userRank/WeeklyRank.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { getCurruntDate, getLastFriday4PM } from "@/utils/time";
 import RankHeader from "./RankHeader";
 import { getMyWeeklyRevenueRank, getWeeklyRevenueRank } from "@/actions/userRank";
@@ -6,23 +8,58 @@ import { weeklyRankDataType } from "@/types/rankBoxType";
 import RankBox from "./RankBox";
 import { isWithinFridayWindow } from "@/utils/aggregateTimeCheck";
 import PopOverBox from "@/components/ui/PopOverBox";
+import { useEffect, useRef, useState } from "react";
+import { m } from "framer-motion";
 
-export default async function WeeklyRank() {
-
+export default function WeeklyRank() {
+  const [myRankInfo, setMyRankInfo] = useState<weeklyRankDataType | null>(null)
+  const [WeeklyRank, setWeeklyRank] = useState<weeklyRankDataType[]>([])
+  const [page, setPage] = useState<number>(0)
+  const observerRef = useRef<HTMLDivElement>(null)
   const date = getLastFriday4PM();
   const isValid = isWithinFridayWindow();
 
-  let myRankInfo = null;
-  let WeeklyRank = [];
+  const fetchRankData = async () => {
+    if(!isValid){
+      const res = await getWeeklyRevenueRank(page);
+      const resData = res?.data?.content;
 
-  if(!isValid){
-    const myRankResponse = await getMyWeeklyRevenueRank();
-    myRankInfo = myRankResponse?.data;
-  
-    const res = await getWeeklyRevenueRank();
-    WeeklyRank = res?.data.content;
+      setWeeklyRank((prev) => {
+        const newRank = resData.filter((rank: weeklyRankDataType) => {
+          return !prev.some((prevRank) => prevRank?.nickname == rank?.nickname)
+        });
+        return [...prev, ...newRank];
+      });
+    }
   }
 
+  const fetchMyRankData = async () => {
+    if(!isValid){
+      const myRankResponse = await getMyWeeklyRevenueRank();
+      setMyRankInfo(myRankResponse?.data ?? null);
+    }
+  }
+
+  const moreData = () => {
+    console.log("데이터 호출")
+    setPage((prev) => prev + 1)
+  };
+  useEffect(() => {
+    fetchMyRankData();
+    fetchRankData();
+  }, [page]);
+
+  useEffect(() => {
+    fetchRankData();
+    const observer = new IntersectionObserver((entries) => {
+      if(entries[0]?.isIntersecting){
+        moreData();
+      }
+    });
+    if(observerRef?.current){
+      observer?.observe(observerRef?.current);
+    }
+  }, [page]);
 
   return(
     <>
@@ -31,11 +68,18 @@ export default async function WeeklyRank() {
       <RankHeader columns={['순위','변동','닉네임','수익률']} /> 
       { isValid ? <div className="text-center my-20">현재는 순위 집계 시간입니다.</div> :
         <>
-          <MyRankBox rank={myRankInfo?.ranking} changingRank={myRankInfo?.rankingChange} nickname={myRankInfo?.nickname} profit={myRankInfo?.profit} isAsseted={false}/>
+        { myRankInfo &&
+          <MyRankBox rank={myRankInfo?.ranking} changingRank={myRankInfo?.changeRanking} nickname={myRankInfo?.nickname} profit={myRankInfo?.profit} isAsseted={false}/>
+        }
           {
-            WeeklyRank.map((data : weeklyRankDataType) =>
-              <RankBox rank={data?.ranking} changingRank={data?.changeRanking} nickname={data?.nickname} profit={data?.profit} isAsseted={false}/>
-            )
+            <section>
+              {
+                WeeklyRank.map((data : weeklyRankDataType) =>
+                  <RankBox rank={data?.ranking} changingRank={data?.changeRanking} nickname={data?.nickname} profit={data?.profit} isAsseted={false}/>
+                )
+              }
+              <div ref={observerRef} />
+            </section>
           }
         </>
       }
