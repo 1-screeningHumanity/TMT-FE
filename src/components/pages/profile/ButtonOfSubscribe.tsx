8c3584ec4574ae868cb0234f9415e77444ee4e4a
jmlee119 <jmlee119@naver.com>
2024-07-03 17:08:39 +0900

    fix :merge error 수정

--- src/components/pages/profile/ButtonOfSubscribe.tsx
+++ src/components/pages/profile/ButtonOfSubscribe.tsx
@@ -1,32 +1,36 @@
 'use client'
 
-import { userInformation } from "@/actions/myPage"
-import { profilePortfolio } from "@/actions/profile"
-import { unsubscribe } from "@/actions/subscribe"
-import { toast, useToast } from "@/components/ui/use-toast"
-import { useRouter } from "next/navigation"
-import { use, useEffect } from "react"
+import { userInformation } from '@/actions/myPage'
+import { profilePortfolio } from '@/actions/profile'
+import { unsubscribe } from '@/actions/subscribe'
+import { toast, useToast } from '@/components/ui/use-toast'
+import { useRouter } from 'next/navigation'
+import { use, useEffect } from 'react'
 
-export default function ButtonOfSubscribe({isSubscribe, nick} : {isSubscribe: boolean ,nick : string}){
-
+export default function ButtonOfSubscribe({
+  isSubscribe,
+  nick,
+}: {
+  isSubscribe: boolean
+  nick: string
+}) {
   const router = useRouter()
-  const { toast } = useToast();
+  const { toast } = useToast()
 
   useEffect(() => {
-    
-    async function fetchData(){
-      const portfolioRes = await profilePortfolio(nick);
-      const portfolio = portfolioRes?.data?.[0]?.nickname;
+    async function fetchData() {
+      const portfolioRes = await profilePortfolio(nick)
+      const portfolio = portfolioRes?.data?.[0]?.nickname
 
-      const userInfoRes = await userInformation();
-      const userInfo = userInfoRes?.data?.nickanme;
+      const userInfoRes = await userInformation()
+      const userInfo = userInfoRes?.data?.nickanme
 
-      if(userInfo == nick){
-        router.push("/mypage");
+      if (userInfo == nick) {
+        router.push('/mypage')
       }
-    
-      if(portfolio == null){
-        router.push("/404");
+
+      if (portfolio == null) {
+        router.push('/404')
       }
     }
     fetchData()
@@ -34,32 +38,43 @@ export default function ButtonOfSubscribe({isSubscribe, nick} : {isSubscribe: bo
 
   const handleUnsubscribe = async () => {
     const res = await unsubscribe(nick)
-    console.log("handleUnsubscribe :", res);
-    if(res.isSuccess){
+    console.log('handleUnsubscribe :', res)
+    if (res.isSuccess) {
       toast({
         title: '구독이 취소되었습니다',
-        variant : "success",
-        action:(<button onClick={() =>  router.refresh()}>확인</button>)
+        variant: 'success',
+        action: <button onClick={() => router.refresh()}>확인</button>,
       })
-    }else{
+    } else {
       toast({
         title: '구독 취소에 실패했습니다',
-        variant : "destructive",
-      })}
+        variant: 'destructive',
+      })
+    }
   }
 
-  console.log("isSubscribe :", isSubscribe);
-  return(
+  console.log('isSubscribe :', isSubscribe)
+  return (
     <>
-      { isSubscribe == false || isSubscribe == undefined ?
-          <div className="flex items-center mx-8">
-            <button className="w-16 h-8 border-[1px] bg-[#7d12ff] rounded-full text-white mt-4" onClick={() => router.push(`/profile/${nick}/payments`)}>구독</button> 
-          </div> 
-          :
-          <div className="flex items-center mx-8">
-            <button className="w-16 h-8 border-[1px] border-[#000000] rounded-full text-black mt-4" onClick={handleUnsubscribe}>구독중</button>
-          </div>
-        }
+      {isSubscribe == false || isSubscribe == undefined ? (
+        <div className="flex items-center mx-8">
+          <button
+            className="w-16 h-8 border-[1px] bg-[#7d12ff] rounded-full text-white mt-4"
+            onClick={() => router.push(`/profile/${nick}/payments`)}
+          >
+            구독
+          </button>
+        </div>
+      ) : (
+        <div className="flex items-center mx-8">
+          <button
+            className="w-16 h-8 border-[1px] border-[#000000] rounded-full text-black mt-4"
+            onClick={handleUnsubscribe}
+          >
+            구독중
+          </button>
+        </div>
+      )}
     </>
   )
 }
