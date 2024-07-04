f354c021548bdb5e1b454e86d78e6cd74efa1a78
sw-devr <sw4689@naver.com>
2024-07-04 12:08:51 +0900

    fix : 내 예수금 조회 api 반환값 optional 처리

--- src/actions/wallet.ts
+++ src/actions/wallet.ts
@@ -4,7 +4,7 @@ import { getAccessToken } from './tokens'
 async function wonInfoAPI() {
   const token = await getAccessToken()
   const res = await GetAPI('/payment/woninfo', undefined, token)
-  return res.data?.won
+  return res?.data?.won
 }
 
 async function cashInfoAPI() {
